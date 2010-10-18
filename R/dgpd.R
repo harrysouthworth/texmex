060dcfa3a3a52d0d47aefc83b9b5ffeb8645f39f
harrys <harrys@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-10-18 18:27:13 +0000

    should fix return level plots and density plots (dgpd)
    git-svn-id: https://texmex.googlecode.com/svn/pkg@11 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/dgpd.R
+++ R/dgpd.R
@@ -1,7 +1,7 @@
 `dgpd` <-
 function(x, sigma = 1, xi = 0, u = 0, log=FALSE ){
 	if ( xi != 0 ){
-		res <- logb(1 + (xi * (x - u))/sigma ) * ( -1/xi - 1)/sigma
+		res <- logb(1 + (xi * (x - u))/sigma ) * ( -1/xi - 1) - log(sigma)
 		res <- ifelse( exp(res) >=0, res, 0 )
 	}
 	else {
--- R/rl.gpd.R
+++ R/rl.gpd.R
@@ -1,91 +1,114 @@
 `rl.gpd` <-
-function( object, alpha = .050,
-		  xlab, ylab, main ,
-		  pch= 1, col =2 , cex=.75, linecol = 4 ,
-		  cicol = 0, polycol = 15, smooth = TRUE ){
-	a = object$mle
-	a[3] <- object$threshold
-	a[ 1 ] = exp( a[ 1 ] )
-	u = object$threshold
-	la = object$rate
-#	n = object$n
-	n <- length( object$y )
-	npy <- 1
-	mat = object$cov
-	dat = object$y
-	# Next line is either a relic or needs updating with X.phi, X.xi
-	xdat = object$xdat
-	
-	a <- c(la, a)
-	eps <- 1e-006
-	a1 <- a2 <- a3 <- a
-	a1[1] <- a[1] + eps
-	a2[2] <- a[2] + eps
-	a3[3] <- a[3] + eps
-	jj <- seq(0, 3.75 + log10(npy), by = 0.1)
-	m <- unique( c(1/la, 10^jj) )
-	q <- qgpd2(m, a[ 2 ], a[ 3 ], u, la)
-	d1 <- (qgpd2( m, a1[ 2 ] , a1[ 3 ], u, la ) - q)/eps
-	d2 <- (qgpd2( m, a2[ 2 ] , a2[ 3 ], u, la) - q)/eps
-	d3 <- (qgpd2( m, a3[ 2 ] , a3[ 3 ], u, la) - q)/eps
-	d <- cbind(d1, d2, d3)
+function(object, alpha = .050,
+         xlab, ylab, main ,
+         pch= 1, col =2 , cex=.75, linecol = 4 ,
+         cicol = 0, polycol = 15, smooth = TRUE ){
 
-	mat <- matrix(c((la * (1 - la))/n, 0, 0, 0, mat[1, 1], mat[1, 2], 0, 
-		mat[2, 1], mat[2, 2]), nc = 3)
-	q.form <- function (d, m) {
-	    t(as.matrix(d)) %*% m %*% as.matrix(d)
-	}
-	v <- apply(d, 1, q.form, m = mat)
+    # Define a helper function. Page 82 of Coles.
+    gpd.delta <- function(a, m){
+        # Jan's original code worked with sigma, not log(sigma)
+        # Need to multiply the second component by exp(phi) = sigma.
+        # This is not exact if a prior (penalty) function is used, but
+        # the CI is approximate anyway.
+        if (length(a) != 3){
+            stop("covariates not allowed in return level plot")
+        }
+        
+        out <- matrix(0, nrow=3, ncol=length(m))
 
-	if ( missing( xlab ) || is.null( xlab ) ) xlab = "Return period"
-	if ( missing( ylab ) || is.null( ylab ) ) ylab = "Return level"
-	if ( missing( main ) || is.null( main ) ) main = "Return Level Plot"
+        # Deal with exponential case
+        if (a[3] == 0){
+            out[1,] <- a[2] / a[1]
+            out[2,] <- log(m * a[1])
+        }
+        else {
+            out[1,] <- a[2] * m^a[3] * a[1]^(a[3] - 1)
+            out[2,] <- 1/a[3] * ((m*a[1])^a[3] - 1) * exp(a[2]) # <--- mult by sigma = e^phi
+            out[3,] <- -a[2] / (a[3]*a[3]) * ( (m * a[1] )^a[3] - 1 ) +
+                       a[2] / a[3] * (m * a[1])^a[3] * log(m * a[1])
+        } # Close else
 
-	plot(m/npy, q,
-		 log = "x",
-		 type = "n",
-		 xlim = c(1, max(m)/npy),
-		 ylim = c(u, max(xdat, q[q > u - 1] + 1.96 * sqrt(v)[q > u - 1])), 
-		 xlab = xlab, ylab = ylab, main = main
-		)
-	# Do polygon and CI lines
-	if ( smooth & length( xdat ) > 2 ) {
-		splo = spline( m[q > u - 1]/npy, q[q > u - 1] - qnorm(1 - alpha/2) * sqrt(v)[q > u - 1] , 200 )
-		sphi = spline( m[q > u - 1]/npy, q[q > u - 1] + qnorm(1 - alpha/2) * sqrt(v)[q > u - 1] , 200 )
-		if ( polycol != 0 )
-			polygon( c( splo$x, rev( sphi$x ) ),
-				     c( splo$y, rev( sphi$y ) ),
-				     col = polycol ,
-					border=FALSE
-				    )
-		lines( splo$x, splo$y, col = cicol )
-		lines( sphi$x, sphi$y, col = cicol )
-	}
-	else{
-		if ( polycol != 0 )
-			polygon( c( m[q > u - 1]/npy, rev( m[q > u - 1]/npy ) ),
-			 		 c( q[q > u - 1] - qnorm(1 - alpha/2) * sqrt(v)[q > u - 1],
-			 		 	rev( q[q > u - 1] + qnorm(1 - alpha/2) * sqrt(v)[q > u - 1] ) ),
-			 		 col=polycol,
-					border = FALSE
-			 		)
-		lines( m[q > u - 1]/npy,
-			   q[q > u - 1] + qnorm(1 - alpha/2) * sqrt(v)[q > u - 1],
-		   	   col = cicol
-		  	 )
-		lines( m[q > u - 1]/npy,
-			   q[q > u - 1] - qnorm(1 - alpha/2) * sqrt(v)[q > u - 1],
-		   	   col = cicol 
-		     )
-	}
+        out
+
+    } # Close gpd.delta
+
+    a <- object$mle
+    u <- object$threshold
+    la <- object$rate
+    n <- length(object$y) / la # Number of obs prior to thresholding
+
+    mat <- object$cov
+    xdat <- object$y
 	
-	lines( m[q > u - 1]/npy, q[q > u - 1],
-		   col = linecol[ 1 ] )
+    a <- c(la, a)
+
+    jj <- seq(-1, 3.75, by = 0.1)
+    m <- unique( c(1/la, 10^jj) )
+
+    q <- qgpd2(m, exp(a[2]), a[3], u, la)
+    d <- t(gpd.delta(a = a, m = m))
+
+    # Get covariance including P(over threshold) parameter
+    mat <- matrix(c(la * (1 - la)/n, 0, 0,
+                  0, mat[1,],
+                  0, mat[2,]), ncol = 3)
+
+    # Get (4.15) of Coles, page 82, adjusted for phi = log(sigma)
+    v <- mahalanobis(d, center=c(0, 0, 0), cov=mat, inverted=TRUE)
+
+    if (missing(xlab) || is.null(xlab)) { xlab <- "Return period" }
+    if (missing(ylab) || is.null(ylab)) { ylab <- "Return level" }
+    if (missing(main) || is.null(main)) { main <- "Return Level Plot" }
+
+    plot(m, q,
+         log = "x",
+         type = "n",
+         xlim = c(1, max(m)),
+         ylim = c(u, max(xdat, q[q > u - 1] + qnorm(1-alpha/2) * sqrt(v)[q > u - 1])), 
+         xlab = xlab, ylab = ylab, main = main)
+
+    # Do polygon and CI lines
+    if (smooth & length(xdat) > 2) {
+        splo <- spline(m[q > u - 1],
+                       q[q > u - 1] - qnorm(1-alpha/2) * sqrt(v)[q > u - 1] ,
+                       200)
+        sphi <- spline(m[q > u - 1],
+                       q[q > u - 1] + qnorm(1-alpha/2) * sqrt(v)[q > u - 1] ,
+                       200)
+        if ( polycol != 0 ) {
+            polygon( c( splo$x, rev( sphi$x ) ),
+	             c( splo$y, rev( sphi$y ) ),
+                     col = polycol ,
+                     border=FALSE		    )
+         } # Close if (polycol
+         lines( splo$x, splo$y, col = cicol )
+         lines( sphi$x, sphi$y, col = cicol )
+    } # Close if (smooth &
+    else{
+        if (polycol != 0){
+            polygon(c(m[q > u - 1], rev( m[q > u - 1])),
+                    c(q[q > u - 1] - qnorm(1 - alpha/2) * sqrt(v)[q > u - 1],
+                      rev(q[q > u - 1] + qnorm(1 - alpha/2) * sqrt(v)[q > u - 1])),
+                    col=polycol,
+                    border = FALSE) # Close polygon
+        } # Close if
+        else {
+            lines(m[q > u - 1],
+                  q[q > u - 1] + qnorm(1 - alpha/2) * sqrt(v)[q > u - 1],
+                  col = cicol)
+            lines(m[q > u - 1],
+                  q[q > u - 1] - qnorm(1 - alpha/2) * sqrt(v)[q > u - 1],
+                  col = cicol)
+        }
+    } # Close else
 	
-	nl <- n - length(dat) + 1
-	sdat <- sort(xdat)
-	points((1/(1 - (1:n)/(n + 1))/npy)[sdat > u], sdat[sdat > u], pch= pch, col = col )	
-	box()
-	invisible()
+    lines(m[q > u - 1], q[q > u - 1],
+          col = linecol[ 1 ] )
+
+    # Add observed data to the plot
+    ly <- length(object$y)
+    points(1 / (1 - ((n - ly + 1):n) / (n + 1)), sort(xdat), pch=pch, col=col)
+    box()
+    invisible()
 }
 
