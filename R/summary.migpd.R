bda892cde9b0c2229f32d40d4064ba92bf5d415c
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-11-03 12:14:25 +0000

    summary.migpd.R: added argument verbose=TRUE so we can switch off printing, primarily for testing purposes.
    git-svn-id: https://texmex.googlecode.com/svn/pkg@16 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/summary.migpd.R
+++ R/summary.migpd.R
@@ -1,26 +1,30 @@
 `summary.migpd` <-
-function( object, ... ){
+function( object, verbose = TRUE, ... ){
 	d <- dim( object$data )[ 2 ]
 	conv <- sapply( object$models, function( z ) z$convergence )
 
 	if ( sum( conv ) != 0 ) 
 		conv <- ( 1:d )[ conv != 0 ]	
 	else conv <- NULL
-
-	print( object$call, ... )
-	cat( "\nA collection of", d, "generalized Pareto models.\n" )
-	if ( is.null( conv ) ) cat( "All models converged.\n" )
-	else cat( "The following model(s) did not converge:", paste( conv, collapse="," ), "\n" )
+  
+  co <- coefficients.migpd( object )
+  
+  if(verbose){
+    print( object$call, ... )
+    cat( "\nA collection of", d, "generalized Pareto models.\n" )
+    
+    if ( is.null( conv ) ) cat( "All models converged.\n" )
+    else cat( "The following model(s) did not converge:", paste( conv, collapse="," ), "\n" )
+    
+    cat( "Penalty to the likelihood:", object$penalty )
+    cat( "\n\nSummary of models:\n" )
+    print( co, ... )
+    cat( "\n" )
 	
-	cat( "Penalty to the likelihood:", object$penalty )
-	
-	cat( "\n\nSummary of models:\n" )
-	co <- coefficients.migpd( object ) 
-	print( co, ... )
-	cat( "\n" )
-	
-	if ( is.null( object$gumbel ) )
-		cat( "Gumbel transformations have not yet been performed.\n" )
+    if ( is.null( object$gumbel ) )
+      cat( "Gumbel transformations have not yet been performed.\n" )
+  }
+  
 	invisible( co )
 }
 
