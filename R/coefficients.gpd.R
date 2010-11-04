90f0bebb87c81b81d09c3f7a2a54a762b92f8139
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-11-04 13:37:38 +0000

    coefficients.gpd: changed $mle to $coefficients for compatibility with R model fitting convention
    git-svn-id: https://texmex.googlecode.com/svn/pkg@27 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/coefficients.gpd.R
+++ R/coefficients.gpd.R
@@ -1,6 +1,6 @@
 `coefficients.gpd` <-
 function( x ) {
-								res = x$mle
+								res = x$coefficients
 								if ( length( res ) == 2 )
 									names( res ) = c( "log(sigma)", "xi" )
 								res
