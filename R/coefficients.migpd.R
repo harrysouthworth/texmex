9313962d8f79708fad667cf27082267cbd834d1f
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-11-04 13:39:02 +0000

    coefficients.migpd: changed $par to $coefficients for compatibility with R model fitting convention
    git-svn-id: https://texmex.googlecode.com/svn/pkg@28 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/coefficients.migpd.R
+++ R/coefficients.migpd.R
@@ -1,10 +1,9 @@
 `coefficients.migpd` <-
 function( x ){
-	co <- sapply( x$models, function( x ) x$par )
+	co <- sapply( x$models, function( x ) x$coefficients )
 	co <- rbind( x$th, x$qu, co )
 	dimnames( co ) <- list( c( "Threshold", "P(X < threshold)", "sigma", "xi" ) ,
 							names( x$models ) )
 	co[ 3, ] <- exp( co[ 3 , ] )
 	co
 }
-
