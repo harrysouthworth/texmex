1a826d3eb2ab5b5e720a7e00a30c9b4c4cbea0ef
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-11-04 13:41:57 +0000

    migpd.R: expunged duplicated god fitting code from this routine and replaced with wrap around to call to "gpd" routine.
    git-svn-id: https://texmex.googlecode.com/svn/pkg@30 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/migpd.R
+++ R/migpd.R
@@ -1,6 +1,6 @@
 `migpd` <-
 function (data, th, qu, penalty = "gaussian", maxit = 10000,
-   verbose = FALSE, priorParameters = NULL)
+   trace = 0, verbose=FALSE, priorParameters = NULL)
 {
    theCall <- match.call()
    d <- dim(data)[2]
@@ -31,7 +31,8 @@ function (data, th, qu, penalty = "gaussian", maxit = 10000,
        else if (any(!is.element(nm, dimnames(data)[[2]])))
            stop("the names of priorParameters must match the column names of the data")
    }
-   fitgpd <- function(i, x, th, penalty, maxit, verbose, priorParameters) {
+   
+   wrapgpd <-function(i, x, th, penalty, maxit, verbose, trace, priorParameters) {
        if (verbose)
            cat("Fitting model", i, "\n")
        if (!is.null(priorParameters))
@@ -39,47 +40,15 @@ function (data, th, qu, penalty = "gaussian", maxit = 10000,
                dimnames(x)[[2]][i]]]]
        x <- c(x[, i])
        th <- th[i]
-       x <- x[x > th]
-       gpd.lik <- function(par, y, th, penalty = "none", priorParameters) {
-           sc <- par[1]
-           xi <- par[2]
-           y <- (y - th)/exp(sc)
-           y <- 1 + xi * y
-           if (min(y) <= 0)
-               l <- 10^6
-           else l <- length(y) * sc + sum(log(y)) * (1/xi +
-               1)
-           if (casefold(penalty) == "none")
-               l <- l
-           else if (casefold(penalty) == "jeffreys") {
-               p <- if (xi <= -0.5)
-                 10^10 * xi
-               else -sc - log(1 + xi) - 0.5 * log(1 + 2 * xi)
-               l <- l - p
-           }
-           else if (casefold(penalty) %in% c("quadratic", "gaussian")) {
-               p <- mahalanobis(matrix(c(sc, xi), nrow = 1),
-                 center = priorParameters[[1]], cov = priorParameters[[2]])
-               l <- l + p
-           }
-           else stop("penalty can be 'none', 'jeffreys' or 'gaussian'")
-           l
+   
+       gpd(x, th=th, penalty=penalty, priorParameters=priorParameters, maxit=maxit, trace=trace)
        }
-#        start <- gpd.pwm(x - th)
-         start <- c(mean(x-th), .01)
-       start[1] <- log(start[1])
-       o <- try(optim(par = start, fn = gpd.lik, y = x, th = th,
-           penalty = penalty, control = list(maxit = maxit),
-           priorParameters = priorParameters))
-       if (class(o) == "try-error" || o$convergence != 0){
-           warning("Non-convergence in migpd")
-       }
-       o
-   }
-   modlist <- lapply(1:d, fitgpd, x = data, penalty = penalty,
-       th = th, maxit = maxit, verbose = verbose, priorParameters = priorParameters)
-   if (length(dimnames(data)[[2]]) == dim(data)[[2]])
+       
+   modlist <- lapply(1:d, wrapgpd, x=data, penalty=penalty, th=th, verbose=verbose,
+                        priorParameters=priorParameters,maxit=maxit,trace=trace)
+   if (length(dimnames(data)[[2]]) == dim(data)[[2]]){
        names(modlist) <- dimnames(data)[[2]]
+       }
    names(th) <- names(qu) <- dimnames(data)[[2]]
    res <- list(call = theCall, models = modlist, data = data,
        th = th, qu = qu, penalty = penalty, priorParameters = priorParameters)
@@ -102,7 +71,7 @@ test(migpd) <- function(){
     th = rep(.7, 5),
     sigma = c(6.2, 9.3, 117.4, 19.7, 37.5),
     xi = c(-.37, -.03, -.09, .11, -.2))
-  
+
   summer.gpd <- summary(migpd(summer, qu=htsummer[2,],penalty="none"),verbose=FALSE)
   winter.gpd <- summary(migpd(winter, qu=htwinter[2,],penalty="none"),verbose=FALSE)
   
