e43d76ee9ccb78e9d488647d501c3eff41360eef
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-11-05 16:57:32 +0000

    bgpd: moved thinning and burn-in routine into separate file thinAndBurn.R; added testing attribute.  Some additional tidying up.
    git-svn-id: https://texmex.googlecode.com/svn/pkg@35 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/bgpd.R
+++ R/bgpd.R
@@ -1,10 +1,13 @@
 `bgpd` <-
 function(y, data, th, qu, phi = ~ 1, xi = ~ 1, prior="gaussian",
-          priorParameters = NULL,
-		  iter = 10500, burn=500, thin = 1 ,
-		  jump.const, # Scale the covar of the jumping dist
-		  start, trace = 1000 ){
+         priorParameters = NULL,
+		     iter = 10500, burn=500, thin = 1 ,
+		     jump.const, # Scale the covar of the jumping dist
+		     start, trace = 1000,
+         verbose = TRUE){
 
+  require(mvtnorm,quietly=verbose)
+  
 	theCall <- match.call()
 
     if (class(try(y, silent=TRUE)) == "try-error"){
@@ -19,7 +22,6 @@ function(y, data, th, qu, phi = ~ 1, xi = ~ 1, prior="gaussian",
         if (missing(th)){
             th <- quantile(y, qu)
         }
-#        y <- data[, y]
         X.phi <- model.matrix(phi, data)
         X.xi <- model.matrix(xi, data)
         X.phi <- X.phi[y > th,]
@@ -50,10 +52,6 @@ function(y, data, th, qu, phi = ~ 1, xi = ~ 1, prior="gaussian",
 
 	u <- th
 	
-	if ( thin < 1 ) thin <- 1 / thin
-	if ( thin %% 1 > 10^(-6) ) stop("thin, or its reciprocal, should be an integer" )
-	if ( burn > iter ) stop( "burn-in is longer that the whole chain" )
-	
 	prior <- casefold(prior)
 	penalty <- prior
 	# Define the LOG-priors
@@ -74,7 +72,7 @@ function(y, data, th, qu, phi = ~ 1, xi = ~ 1, prior="gaussian",
     if (!is.null(priorParameters)){
         dimp <- ncol(X.phi) + ncol(X.xi)
         if (length(priorParameters[[1]]) != dimp){
-            stop("wrong number of parameters in prior (doesn't match phi and xi formulas)")
+            stop("wrong number of location parameters in prior (doesn't match phi and xi formulas)")
         }
         else if (length(diag(priorParameters[[2]])) != dimp){
             stop("wrong dimension of prior covariance (doesn't match phi and xi formulas)")
@@ -115,18 +113,19 @@ function(y, data, th, qu, phi = ~ 1, xi = ~ 1, prior="gaussian",
 
 	res <- matrix( ncol=ncol(X.phi) + ncol(X.xi), nrow=iter )
 
-    if ( missing( start ) ) res[ 1 , ] <- coef( mod )
-	else res[ 1 , ] <- start
+  if ( missing( start ) ) 
+    res[ 1 , ] <- coef( mod )
+	else 
+    res[ 1 , ] <- start
 
-#    x <- x[data > u]
-#	data <- data[data > u] - u
-
-        if (!exists(".Random.seed")){ runif(1) }
+  if (!exists(".Random.seed")) runif(1) 
 	seed <- .Random.seed # Retain and add to output
 
 	for( i in 2:iter ){
-		if( i %% trace == 0 ) cat(i, " steps taken\n" )
-		if (is.R() ){
+    if( verbose){
+      if( i %% trace == 0 ) cat(i, " steps taken\n" )
+    }
+		if ( exists("is.R") && is.function(is.R) && is.R() ){
 			prop <- c(rmvnorm( 1 , mean = coef(mod), sigma = mod$cov * jump.const ))
 		}
 		else {
@@ -146,28 +145,135 @@ function(y, data, th, qu, phi = ~ 1, xi = ~ 1, prior="gaussian",
 	if ( trace < iter ){
 		cat( "Acceptance rate:", round( acc , 3 ) , "\n" )
 	}
-
-    if (burn > 0){
-  	    param <- res[ -( 1:burn ) , ] # Remove burn-in
-  	}
-	wh <- 1:dim( param )[[ 1 ]] %% thin == 0
-	param <- param[ wh , ]
-
-	# Simulate probabilities of being over threshold
-#	if ( mean( y > u , na.rm = TRUE ) == 1 ) p <-1
-#	else
-#		p <- rbeta( dim( param )[[ 1 ]], .5 + sum( mod$xdata > u , na.rm=TRUE ),
-#											.5 + sum( mod$xdata <= u , na.rm = TRUE)
-#					)
 	
 	res <- list( call=theCall, threshold=u , map = mod ,
-#                    p=p,
 					burn = burn, thin = thin, 
-					chains=res , param=param,
+					chains=res ,
 					X.phi = X.phi, X.xi = X.xi,
 					acceptance=acc, seed=seed 
 					)
-	oldClass( res ) <- "bgpd"
+  oldClass( res ) <- "bgpd"
+  res <- thinAndBurn(res)
 	invisible( res )
 }
 
+#*************************************************************
+#*************************************************************
+#*************************************************************
+# TESTING ROUTINE
+
+test(bgpd) <- function(){
+  require(svUnit,quiet=TRUE)
+  
+  postSum <- function(x){
+    t(apply(x$param, 2, function(o){ c(mean=mean(o), se=sd(o)) }))
+  } 
+  
+#************************************************************* 
+# 4.1. Test reproducibility
+
+  runif(1)
+  save.seed <- .Random.seed
+
+  set.seed(save.seed)
+  bmod <- bgpd(ALT_M, data=liver, th=quantile(liver$ALT_M, .7), iter=1000,verbose=FALSE)
+  
+  set.seed(save.seed)
+  bmod2 <- bgpd(ALT_M, data=liver, th=quantile(liver$ALT_M, .7), iter=1000,verbose=FALSE)
+  
+  checkEqualsNumeric(bmod$param,bmod2$param)
+
+  set.seed(bmod$seed)
+  bmod3 <- bgpd(ALT_M, data=liver, th=quantile(liver$ALT_M, .7), iter=1000,verbose=FALSE)
+  checkEqualsNumeric(bmod$param, bmod3$param)
+
+#*************************************************************  
+# 4.2. Logical test of burn-in
+
+  checkEqualsNumeric(nrow(bmod$param), nrow(bmod$chains) - bmod$burn)
+
+  iter <- sample(500:1000,1)
+  burn <- sample(50,1)
+  bmod2 <- bgpd(ALT_M, data=liver, th=quantile(liver$ALT_M, .7),
+                iter=iter, burn=burn,verbose=FALSE)
+
+  checkEqualsNumeric(nrow(bmod2$param), iter-burn)
+
+#*************************************************************
+# 4.3. Logical test of thinning
+
+  thin <- 0.5
+  iter <- 1000
+  bmod <- bgpd(ALT_M, data=liver, th=quantile(liver$ALT_M, .7),
+               iter=iter, thin = thin,verbose=FALSE)
+
+  checkEqualsNumeric(nrow(bmod$param), (nrow(bmod$chains) - bmod$burn) * thin,msg="1")
+
+  thin <- 2
+  iter <- 1000
+  bmod <- bgpd(ALT_M, data=liver, th=quantile(liver$ALT_M, .7),
+               iter=iter, thin = thin,verbose=FALSE)
+
+  checkEqualsNumeric(nrow(bmod$param), (nrow(bmod$chains) - bmod$burn) / thin,msg="2")
+
+#*************************************************************  
+## Checks of bgpd. Centres of distributions and standard deviations 
+## should be similar to those coming out of gpd with the same penalty.
+## Posterior means are not the same as MAPs and SEs from Hessian are
+## approximations, so allow some tolerance.
+
+  tol <- 0.05
+  tol.se <- 0.3
+# 4.4. Compare MAP and posterior summaries for simple model
+
+  mod <- gpd(ALT_M, data=liver, qu=.7)
+  bmod <- bgpd(ALT_M, data=liver, th=quantile(liver$ALT_M, .7),verbose=FALSE)
+
+  checkEqualsNumeric(postSum(bmod)[,1],coef(mod), tolerance=tol)
+  checkEqualsNumeric(postSum(bmod)[,2],mod$se, tolerance=tol.se)
+
+#*************************************************************
+# 4.5. Covariates in phi
+
+  require(MASS,quiet=TRUE) # Use MASS because is ships with R and has no dependencies
+              # that don't ship with R.
+  liver <- liver
+  liver$ndose <- as.numeric(liver$dose)
+
+# function rlm : Fit a linear model by robust regression using an M estimator:
+
+  rmod <- rlm(log(ALT_M) ~ log(ALT_B) + dose, data=liver) 
+  liver$resids <- resid(rmod)
+
+  mod <- gpd(resids, data=liver, qu=.7, phi = ~ ndose)
+
+  bmod <- bgpd(resids, data=liver, th=quantile(liver$resids, .7),
+               phi = ~ ndose,verbose=FALSE)
+
+  checkEqualsNumeric(postSum(bmod)[,1],coef(mod), tolerance=tol)
+  checkEqualsNumeric(postSum(bmod)[,2],mod$se, tolerance=tol.se)
+
+#*************************************************************
+# 4.6. Covariates in xi
+
+  mod <- gpd(resids, data=liver, qu=.7, xi = ~ ndose)
+  bmod <- bgpd(resids, data=liver, th=quantile(liver$resids, .7),
+               xi = ~ ndose,verbose=FALSE)
+
+  checkEqualsNumeric(postSum(bmod)[,1],coef(mod), tolerance=tol)
+  checkEqualsNumeric(postSum(bmod)[,2],mod$se, tolerance=tol.se)  
+
+#*************************************************************
+# 4.7. Covariates in xi and phi
+
+  mod <- gpd(resids, data=liver, qu=.7,
+             xi = ~ ndose, 
+             phi = ~ ndose)
+  bmod <- bgpd(resids, data=liver, th=quantile(liver$resids, .7),
+               xi = ~ ndose, 
+               phi = ~ ndose,verbose=FALSE)
+
+  checkEqualsNumeric(postSum(bmod)[,1],coef(mod), tolerance=tol)
+  checkEqualsNumeric(postSum(bmod)[,2],mod$se, tolerance=tol)  
+ 
+}
