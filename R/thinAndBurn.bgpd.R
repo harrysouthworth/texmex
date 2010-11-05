267c9319b0e4b3b6e9cea8820889e2355813addc
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-11-05 16:54:23 +0000

    thinAndBurn.bgpd moved thinning and burn-in code out of bgpd function
    git-svn-id: https://texmex.googlecode.com/svn/pkg@34 e3c61688-98f1-767d-8bab-d104bf98bfd2

---
+++ R/thinAndBurn.bgpd.R
@@ -0,0 +1,90 @@
+thinAndBurn <- function (object, ...)
+  UseMethod("thinAndBurn")
+
+thinAndBurn.bgpd <- function(obj,burn,thin){
+
+  if(missing(burn)){
+    burn <- obj$burn
+  } else {
+    obj$burn <- burn
+  }
+  if(missing(thin)){
+    thin <- obj$thin
+  } else {
+    obj$thin <- thin
+  }
+  if(is.null(obj$thin)){
+    stop("thin or its reciprocal must be a positive integer, for no thinning use thin=1")
+  }     
+  if(is.null(obj$burn)){
+    stop("burn must be a non-negative integer, for no burn in use burn=0")
+  }     
+  
+ 	if ( thin < 1 ) thin <- 1 / thin
+	if ( thin %% 1 > 10^(-6) ) stop("thin, or its reciprocal, should be an integer" )
+	if ( burn > dim(obj$chains)[1] ) stop( "burn-in is longer that the whole chain" )
+
+  if (burn > 0){
+     obj$param <- obj$chains[ -( 1:burn ) , ] # Remove burn-in
+  } else {
+     obj$param <- obj$chains
+  }
+	wh <- 1:dim( obj$param )[[ 1 ]] %% thin == 0
+	obj$param <- obj$param[ wh , ]
+
+  invisible(obj)
+}
+
+test(thinAndBurn.bgpd) <- function(){
+require(svUnit)
+
+# generate data to use for checking
+  d <- sample(3:10,1)
+  nrow <- 100
+  x <- list(chains = apply(matrix(rep(1:d,each=nrow),ncol=d),2, function( o ) o*1:nrow))
+  oldClass( x ) <- "bgpd" 
+
+# test appropriate errors for misspecification of thin and burn
+
+  checkException(thinAndBurn(x,burn=2))
+  checkException(thinAndBurn(x,thin=1))
+  
+#  test burn in  
+  burn <- sample(nrow/2,1)
+  burnOnly <- thinAndBurn(x,burn=burn,thin=1)
+  checkEqualsNumeric(burnOnly$param[1,], x$chains[burn+1,])
+
+# test thinning
+  thin <- 2
+  thinOnly <- thinAndBurn(x,thin=thin,burn=0)
+
+  checkEqualsNumeric(thinOnly$param[,1], seq(thin,nrow,by=thin))
+  
+# test thinning and burning simultaneously
+
+  thinBurn <- thinAndBurn(x,thin=thin,burn=burn)
+  
+  checkEqualsNumeric(thinBurn$param[,1], seq(burn + thin, nrow,by=thin))
+  
+# test returned values of thin and burn
+
+  checkEqualsNumeric(thinBurn$thin,thin,burn=0)
+  checkEqualsNumeric(thinBurn$burn,burn,thin=1)
+  
+# test passing thin and burn via object
+
+  x$thin <- thin
+  x$burn <- burn
+  thinBurn1 <- thinAndBurn(x)
+  
+  checkEqualsNumeric(thinBurn1$param[,1], seq(burn + thin, nrow,by=thin))
+  checkEqualsNumeric(dim(thinBurn$param),dim(thinBurn1$param))
+  
+# test thinning and burning a previously thinned and burned object
+
+  thin2 <- 4
+  burn2 <- 4
+  thinBurn2 <- thinAndBurn(thinBurn1,thin=thin2,burn=burn2)
+  checkEqualsNumeric(dim(thinBurn1$chains)[2], dim(thinBurn2$chains)[2])
+  checkEqualsNumeric(dim(thinBurn2$param)[1],(nrow - burn2) / thin2)
+}
