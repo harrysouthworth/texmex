559c4168940700ea9d858b66c9322e6a354c6f39
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-11-11 11:17:18 +0000

    rgpd: added test attribute; got rid of default values
    git-svn-id: https://texmex.googlecode.com/svn/pkg@43 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/rgpd.R
+++ R/rgpd.R
@@ -1,26 +1,78 @@
-`rgpd` <-
-function(n, sigma = 1, xi = 0 , u = 0 ){
+`rgpd` <- function(n, sigma, xi, u = 0 ){
 
 	# Check parameter vectors have correct length
     sigma <- rep(sigma, length=n)
     xi <- rep(xi, length=n)
     u <- rep(u, length=n)
 
-    # Get random numbers
-    if ( all( xi == 0 ) ){
+  # Get random numbers
+  if ( all( xi == 0 ) ){
 		res <- rexp( n, 1/sigma )+ u
-	}
-	else if (any(xi == 0)){
+	}	else if (any(xi == 0)){
 	    res <- numeric(n)
 
 	    wh <- xi == 0
 	    res[wh] <- rexp(sum(xi == 0), 1/sigma[wh]) + u[wh]
 
 	    res[!wh] <- sigma[!wh]/xi[!wh] * (runif(sum(!wh))^(-xi[!wh]) - 1) + u[!wh]
-	}
-	else {
+	}	else {
 		res <- u + sigma / xi  * (runif(n)^( -xi ) - 1)
-    }
-    res
+  }
+  res
 }
 
+test(rgpd) <- function(){
+
+  require(evd,quiet=TRUE)
+  edgpd <- get("dgpd",pos=2)
+  eqgpd <- get("qgpd",pos=2)
+  epgpd <- get("pgpd",pos=2)
+  ergpd <- get("rgpd",pos=2)
+  detach(2)
+
+  myTest <- function(seed,p, thresh,msg=""){
+      set.seed(seed)
+      x <- sapply(1:nreps, function(i)rgpd(nsim, p[i,1], p[i,2], u=thresh[i]))
+      set.seed(seed)  
+      ex <- sapply(1:nreps, function(i)ergpd(nsim, loc=thresh[i], scale=p[i,1],shape=p[i,2]))
+      checkEqualsNumeric(ex,x,msg=msg)
+      }
+  seed <- 20101111
+#*************************************************************
+# 6.1. Test rgpd. Note that ergpd is NOT vectorized.
+
+  nreps <- 100
+  nsim <- 1000
+  p <- matrix(runif(2*nreps, -1, 1),ncol=2) 
+  p[, 1] <- p[, 1] + 1
+  thresh <- rep(0,nreps) 
+  
+  myTest(seed,p,thresh=thresh,msg="rgpd: random parameters, zero threshold")
+  
+ #*************************************************************
+# 6.1a Test rgpd with non-zero threshold. Note that ergpd is NOT vectorized.
+
+  nonZeroThresh <- rnorm(nreps)
+  myTest(seed,p,thresh=nonZeroThresh,msg="rgpd: Non-zero threshold")
+  
+#######################################################################
+# 6.2. Test rgpd when some or all xi == 0. Note that ergpd is NOT vectorized.
+
+  p[sample(1:nreps,nreps/2),2] <- 0
+  myTest(seed,p,thresh=thresh,msg="rgpd: some zero xi")
+  p[,2] <- 0
+  myTest(seed,p,thresh=thresh,msg="rgod: all zero xi")
+
+#######################################################################
+# 6.3. Test vectorization of rgpd. ergpd is NOT vectorized
+
+  sig <- runif(nreps, 0, 2)
+  xi <- runif(nreps)
+
+  set.seed(seed)
+  x <- rgpd(nreps, sig, xi)
+  set.seed(seed)
+  ex <- sapply(1:nreps, function(i)ergpd(1, loc=0, scale=sig[i], shape=xi[i]))
+  
+  checkEqualsNumeric(ex, x, msg="rgpd: vectorisation")
+}
