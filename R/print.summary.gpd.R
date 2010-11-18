a58d9be1b02db8cb9e2d41fc274b8daadbd9e71c
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-11-18 10:59:45 +0000

    print.summary.gpd : added dig=3 argument to function definition
    git-svn-id: https://texmex.googlecode.com/svn/pkg@63 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/print.summary.gpd.R
+++ R/print.summary.gpd.R
@@ -1,11 +1,11 @@
 `print.summary.gpd` <-
-function( x, ... ){
+function( x, dig = 3 , ... ){
 	print( x$model , ... )
 	cat( paste( x$nsim, " simulated data sets compared against observed data QQ-plot.\n", sep = "" ) )
 	cat( "Quantile of the observed MSE: ", round( x$envelope$Q, ... ), "\n" )
 	out = sum( x$envelope$data < x$envelope$envelope[1, ] | x$envelope$data > x$envelope$envelope[2, ] )
 	level = paste( round( 100 - x$alpha*100 , ...), "%", sep = "" )
-	perc = round( out / length( x$envelope$data ) * 100, ... )
+	perc = round( out / length( x$envelope$data ) * 100, dig=dig, ... )
 	perc = paste( perc, "%", sep = "" )
 	cat( paste( out, " observations (", perc, ") outside the ", level, " simulated envelope.\n" , sep="") )
 	invisible()
