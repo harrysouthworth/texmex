4f48163a648da3b81c09658ee5282bd04abf4c17
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-11-18 10:57:59 +0000

    show.summary.cmvxPrediction  : now takes definition from print.summary.cmvxPrediction to avoid code duplication
    git-svn-id: https://texmex.googlecode.com/svn/pkg@61 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/show.summary.cmvxPrediction.R
+++ R/show.summary.cmvxPrediction.R
@@ -1,15 +1,1 @@
-`show.summary.cmvxPrediction` <-
-function( x ){
-	print( x$call )
-		cat( "\nResults from", length( x$B ), "bootstrap runs.\n" )
-	cat( paste( "\nConditioned on ", x$which, " being above its ", 100*x$pqu, "th percentile.\n\n", sep = "" ) )
-	cat( "\nExpected values:\n\n" )
-	print( x$ans )
-
-	cat( "\nConditional probability of threshold exceedance:\n\n" )
-
-	print( x$thres )
-	
-	invisible()
-}
-
+`show.summary.cmvxPrediction` <- print.summary.cmvxPrediction
