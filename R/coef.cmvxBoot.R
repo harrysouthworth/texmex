b8ec8aad18f763598971a07d12534c5cd85eab14
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-11-19 09:39:35 +0000

    coef.cmvxBoot: taken out facility to print marginal parameter estimates (argument "which" deleted); taken out bias correction as this not sensible for parameters from a mixture distribution as a and b often are.
    git-svn-id: https://texmex.googlecode.com/svn/pkg@64 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/coef.cmvxBoot.R
+++ R/coef.cmvxBoot.R
@@ -1,18 +1,10 @@
 `coef.cmvxBoot` <-
-function( x, which="gpd" ){
-	if ( casefold( which ) == "gpd" | which == 1 ) which <- 1
-	else which <- 2
-
-	d2 <- dim( x$boot[[ 1 ]][[ which ]] )
+function( x ){
+	d2 <- dim( x$boot[[ 1 ]][[ 2 ]] )
 	
 	B <- x$B
 	
-    if (which == 1){
-        sco <- x$simpleMar[3:4, ] # Point estimates from (penalized) mle
-    }
-	else {
-	    sco <- x$simpleDep # Point estimates of dependence structure
-	}
+  sco <- x$simpleDep # Point estimates of dependence structure
 	
 	x <- x$boot
 	wh <- unlist( lapply( x , function( z ) dim( z$Z )[[ 1 ]] == dim( na.omit( z$Z ) )[[ 1 ]] ) )
@@ -20,15 +12,10 @@ function( x, which="gpd" ){
 	
 	eff <- sum( wh )
 	
-	co <- unlist( lapply( x , function( z, wh ) z[[ wh ]], wh=which ) )
+	co <- unlist( lapply( x , function( z ) z[[ 2 ]]) )
 	co <- array( co, dim = c( d2[ 1 ] , d2[ 2 ] , length( co ) / prod( d2 ) ) )
 	mco <- apply( co, c( 1, 2 ), mean )
 	seco <- apply( co, c( 1 , 2 ), function( x ) sqrt( var( x ) ) )
-
-    # Mean of bootstrap estimates can be used to estimate bias.
-    # Then need to subtract bias from parameter estimates.
-    b <- mco - sco
-    mco <- sco - b
 
 	res <- list( bPoint = mco , se = seco )
 
@@ -36,7 +23,7 @@ function( x, which="gpd" ){
 							dimnames( x ) <- nms
 							x
 						 } ,
-						 nms = dimnames( x[[ 1 ]][[ which ]] )
+						 nms = dimnames( x[[ 1 ]][[ 2 ]] )
 				 )
 
 	attr( res , "Samples generated" ) <- B
