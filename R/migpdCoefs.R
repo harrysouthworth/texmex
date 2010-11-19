9c3d3d6304532449a0e094c3b67476fcedbc1999
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-11-19 11:42:43 +0000

    migpdCoefs: added test attribute to function definition.
    git-svn-id: https://texmex.googlecode.com/svn/pkg@75 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/migpdCoefs.R
+++ R/migpdCoefs.R
@@ -14,12 +14,58 @@ function(object, which, coefs){
   }
   
   if (length(which) == 1){
-    object$models[which]$par <- coefs
+    object$models[which]$coefficients <- coefs
   }
   
   for (i in 1:length(which)){
-    object$models[[i]]$par <- coefs[[i]]
+    object$models[[i]]$coefficients <- coefs[[i]]
   }
-  
   object
+}
+
+test(migpdCoefs) <- function(){
+
+  liver <- liver
+  liver$ndose <- as.numeric(liver$dose)
+  require(MASS,quiet=TRUE) # For rlm
+
+  ralt <- resid(rlm(log(ALT_M) ~ log(ALT_B) + ndose, data=liver))
+  rast <- resid(rlm(log(AST_M) ~ log(AST_B) + ndose, data=liver))
+  ralp <- resid(rlm(log(ALP_M) ~ log(ALP_B) + ndose, data=liver))
+  rtbl <- resid(rlm(log(TBL_M) ~ log(TBL_B) + ndose, data=liver))
+
+  rliver <- data.frame(alt=ralt, ast=rast, alp=ralp, tbl=rtbl, ndose=liver$ndose)
+
+  Dmod <- migpd(rliver[rliver$ndose == 4, 1:4], qu=.7) # Model for dose D
+
+  oldALTco <- coef(Dmod)[3:4, 1]
+
+  altgpd <- gpd(alt, qu=.7, xi = ~ ndose, data=rliver)
+  astgpd <- gpd(ast, qu=.7, xi = ~ ndose, data=rliver)
+
+  altco <- c(coef(altgpd)[1], coef(altgpd)[2] + 4 * coef(altgpd)[3])
+  astco <- c(coef(astgpd)[1], coef(astgpd)[2] + 4 * coef(astgpd)[3])
+
+# Change one set of coefficients
+  lmod <- migpdCoefs(Dmod, which="alt", list(altco))
+
+  newALTco <- coef(lmod)[3:4, 1]
+  newALTco[1] <- log(newALTco[1]) # For comparison with altco
+  oldALTco[1] <- log(oldALTco[1])
+
+  checkEqualsNumeric(altco, newALTco, msg="migpdCoefs: change one set of coefficients")
+  checkTrue(newALTco != oldALTco, msg="migpdCoefs: change one set of coefficients")
+
+# Change 2 sets of coefficients at once
+
+  lmod <- migpdCoefs(Dmod, which=c("alt", "ast"), coef=list(altco, astco))
+
+  newCo <- coef(lmod)[3:4, 1:2]
+  oldCo <- coef(Dmod)[3:4, 1:2]
+
+  newCo[1,] <- log(newCo[1,])
+  oldCo[1,] <- log(oldCo[1,])
+
+  checkEqualsNumeric(cbind(altco, astco),newCo, msg="migpdCoefs: change two set of coefficients at once")
+  checkTrue(newCo != oldCo, "migpdCoefs: change two set of coefficients at once")
 }
