9dcf68191b249426df0c572d8bfcda1bfbc05ccd
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-11-19 09:57:58 +0000

    print.summary.cmvxPrediction: fixed bug in printing number of bootstrap samples
    git-svn-id: https://texmex.googlecode.com/svn/pkg@70 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/print.summary.cmvxPrediction.R
+++ R/print.summary.cmvxPrediction.R
@@ -1,7 +1,7 @@
 `print.summary.cmvxPrediction` <-
 function( x, ... ){
 	print( x$call, ... )
-		cat( "\nResults from", length( x$B ), "bootstrap runs.\n" )
+  cat( "\nResults from", x$B, "bootstrap runs.\n" )
 	cat( paste( "\nConditioned on ", x$which, " being above its ", 100*x$pqu, "th percentile.\n\n", sep = "" ) )
 	cat( "\nExpected values:\n\n" )
 	print( x$ans, ... )
