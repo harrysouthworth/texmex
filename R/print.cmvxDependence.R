7fa78b7e464dea568fa563dd9355fe406466e1b0
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-11-19 13:49:49 +0000

    print.cmvsDependence: added message saying which variable we are conditioning on
    git-svn-id: https://texmex.googlecode.com/svn/pkg@77 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/print.cmvxDependence.R
+++ R/print.cmvxDependence.R
@@ -1,6 +1,7 @@
 `print.cmvxDependence` <-
 function( x, ... ){
 	print( x$call, ... )
+  cat("\nConditioning on ",x$conditioningVariable," variable.\n", sep="")
 	names( x$gqu ) <- dimnames( x$parameters )[[ 2 ]]
 	cat( "\nThresholding quantiles for Gumbel-transformed data:\n" )
 	print( x$gqu, ... )
