982d5ea7e6992fd4aa4d5139216f3ebb5e620228
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-11-19 09:42:03 +0000

    print.cmvxBoot: followed through consequences of removal from coef.cmvxBoot of marginal parameter printing facility.  Now just prints dependence parameters.
    git-svn-id: https://texmex.googlecode.com/svn/pkg@65 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/print.cmvxBoot.R
+++ R/print.cmvxBoot.R
@@ -1,18 +1,14 @@
 `print.cmvxBoot` <-
 function( x , ... ){
 	print( x$call )
-	cat( paste( "\n", x$B, " bootstrap samples created.\n" , sep = "" ) )
-		
-	co <- coef( x, which = 1 )
-	eff <- attributes( co )$"Effective samples"
-	
-#	cat( paste( eff, "effective samples.\n" ) )
-	
-	cat( "\nMarginal GPD bootstrap parameter estimates (bias adjusted):\n" )
-	print( co[[ 1 ]] )
-	
-	co <- coef( x, which = 2 )[[ 1 ]]
-	cat( "\nDependence structure bootstrap parameter estimates (bias adjusted):\n" )
-	print( co )
-}
+	cat( paste( "\n", x$B, " bootstrap samples created.\n\n" , sep = "" ) )
+
+  co <- coef( x )
+  eff <- attributes( co )$"Effective samples"
+  cat( paste("Dependence structure estimation successful for", eff, "effective samples.\n" ) )
+  cat( paste("Dependence structure bootstrap mean parameter estimates:\n" ))
+  print( co[[ 1 ]] )
+  cat("\n")
+
+	}
 
