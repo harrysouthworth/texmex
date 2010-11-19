eab0abac9465a9372f6b9a87e9b9db963e981d13
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-11-19 09:46:43 +0000

    cmvxPrediction: defined local function makeYsubMinusI to avoid duplication of code; fixed bug reproducing Table 5 in Heffernan and Tawn - previously not using bootstrap parametr estimates for conditioning variable in calculations of conditional expectations.
    git-svn-id: https://texmex.googlecode.com/svn/pkg@68 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/cmvxPrediction.R
+++ R/cmvxPrediction.R
@@ -12,6 +12,22 @@ function( migpd , boot, pqu = .99, nsim = 1000 ){
 		which <- match( which, dimnames( migpd$data )[[ 2 ]] )
 	
 	################################################################
+  makeYsubMinusI <- function( i, z, v , y ){
+			v <- v[ , i ]
+			z <- z[ , i ]
+			if ( !is.na( v[ 1 ] ) ){
+				if( v[ 1 ] < 10^(-5) & v[ 2 ] < 0 ){
+					if( v[ 4 ] < 10^(-5 ) ) d <- 0
+					else d <- v[ 4 ]
+					a <- v[ 3 ] - d * log( y )
+				}
+				else a <- v[ 1 ] * y
+			} # close if( !is.na...
+			else a <- NA
+			a + ( y^v[ 2 ] ) * z
+		}
+  
+	################################################################
 	# The function lfun does most of the work
 	lfun <- function( i , bo, pqu, nsim , migpd, which ){
 		if ( i %% 10 == 0 ) cat( i, "sets done\n" )
@@ -25,32 +41,16 @@ function( migpd , boot, pqu = .99, nsim = 1000 ){
 
 		# Step 3 - get y_{-i}
 		ab <- bo[[ i ]]$dependence
-
-		fun <- function( i, z, v , y ){
-			v <- v[ , i ]
-			z <- z[ , i ]
-			if ( !is.na( v[ 1 ] ) ){
-				if( v[ 1 ] < 10^(-5) & v[ 2 ] < 0 ){
-					if( v[ 4 ] < 10^(-5 ) ) d <- 0
-					else d <- v[ 4 ]
-					a <- v[ 3 ] - d * log( y )
-				}
-				else a <- v[ 1 ] * y
-			} # close if( !is.na...
-#			else a <- NA
-			a + ( y^v[ 2 ] ) * z
-		}
-		ymi <- sapply( 1:( dim( z )[[ 2 ]] ) , fun, z=z, v=ab , y=y )
+		
+		ymi <- sapply( 1:( dim( z )[[ 2 ]] ) , makeYsubMinusI, z=z, v=ab , y=y ) # y_{-i}
 
 		# Step 4 - transform to original scale
 		# Transform to (0, 1)
 		# Do xi
-		xp <- pqu * ( 1 - migpd$qu[ which ] )
-		cox <- coef( migpd )[, which ]
-		m <- 1 / ( 1 - pqu ) # Need to estimate qpu quantile
-		zeta <- 1 - migpd$qu[ which ] # Coles, page 81
-		xth <- migpd$th[ which ] + cox[ 3 ] / cox[ 4 ] * ( ( m*zeta )^cox[ 4 ] - 1 )
-		xi <- u2gpd( exp( -exp( -y ) ), p = 1 - pqu, th=xth, sigma=cox[ 3 ], xi = cox[ 4 ] )
+    cox <- bo[[i]]$GPD[,which]
+		xth <- migpd$th[ which ]
+    xp <- migpd$qu[ which ]
+		xi <- u2gpd( exp( -exp( -y ) ), p = 1 - xp, th=xth, sigma=cox[ 1 ], xi = cox[ 2 ] )
 
 		xmi <- apply( ymi , 2, function( x ) exp( -exp( -x ) ) )
 
@@ -73,8 +73,8 @@ function( migpd , boot, pqu = .99, nsim = 1000 ){
 				   migpd=migpd, pqu=pqu, bo = boot$boot, nsim=nsim,
 				   which = which )
 	
-	# bootRes contains the bootstrap simulated Y_{-i}
-
+	# bootRes contains the bootstrap simulated complete vectors X on the original scale of the data, conditional on having the _which_ component above the pqu quantile.
+  
 	##########################################################################
 	# Also get a sample using the point estimates of the parameters
 	# that are suggested by the data
@@ -82,38 +82,26 @@ function( migpd , boot, pqu = .99, nsim = 1000 ){
 		
 	# Step 2 - get z
 	dall <- cmvxDependence( migpd , which=which , gqu=boot$gqu )
-	dco <- dall$parameters
+	dco <- dall$coefficients
 	z <- dall$Z
 	# Resample rows of Z
 	z <- z[ sample( 1:( dim( z )[ 1 ] ), size=nsim, replace=TRUE ) ,]
 
 	# Step 3 - get y_{-i}
-	fun <- function( i, z, v , y ){
-		v <- v[ , i ]
-		z <- z[ , i ]
-		if( v[ 1 ] < 10^(-5) & v[ 2 ] < 0 ){
-			if( v[ 4 ] < 10^(-5 ) ) d <- 0
-			else d <- v[ 4 ]
-			a <- v[ 3 ] - d * log( y )
-		}
-		else a <- v[ 1 ] * y
-		a + ( y^v[ 2 ] ) * z
-	}
-	ymi <- sapply( 1:( dim( z )[[ 2 ]] ) , fun, z=z, v=dco , y=y )
-	# Checked dim( ymi ) - ok
-
+	ymi <- sapply( 1:( dim( z )[[ 2 ]] ) , makeYsubMinusI, z=z, v=dco , y=y )
+	
 	# Step 4 - transform to original scale
 	# Transform to (0, 1)
 	xi <- exp( -exp( -y ) )
 	xmi <- apply( ymi, 2, function( x ) exp( -exp( -x ) ) )
 
 	# Do xi
-	xp <- pqu * ( 1 - migpd$qu[ which ] )
+	#xp <- pqu * ( 1 - migpd$qu[ which ] )
 	cox <- coef( migpd )[, which ]
 	m <- 1 / ( 1 - pqu ) # Need to estimate qpu quantile
-	zeta <- 1 - migpd$qu[ which ] # Coles, page 81
+ 	zeta <- 1 - migpd$qu[ which ] # Coles, page 81
 	xth <- migpd$th[ which ] + cox[ 3 ] / cox[ 4 ] * ( ( m*zeta )^cox[ 4 ] - 1 )
-	xi <- u2gpd( xi, p = 1 - pqu, th=xth, sigma=cox[ 3 ], xi = cox[ 4 ] )
+	xi <- u2gpd( xi, p = 1 - migpd$qu[ which ], th=migpd$th[ which ], sigma=cox[ 3 ], xi = cox[ 4 ] )
 	
 	# Now need to transform each column of xmi
 	for( i in 1:( dim( xmi )[[ 2 ]] ) ){
