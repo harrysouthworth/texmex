32e0c4b5fd7cbf9dc09c3592087a528ef85cf69f
harrys <harrys@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-11-24 14:44:21 +0000

    fixed funny problem in summary.bgpd. Resorted to using unclass
    git-svn-id: https://texmex.googlecode.com/svn/pkg@87 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/summary.bgpd.R
+++ R/summary.bgpd.R
@@ -1,13 +1,15 @@
 summary.bgpd <- function(x){
+   nms <- names(x$map$coefficients)
    co <- apply( x$param, 2, mean )
    se <- apply(x$param, 2, sd)
    res <- cbind(co, se)
-   dimnames(res) <- list(names(co), c("Posterior mean", "SD"))
+   dimnames(res) <- list(nms, c("Posterior mean", "SD"))
    oldClass(res) <- "summary.bgpd"
    res
 }
 
 print.summary.bgpd <- function(x){
-   print(x)
+   print(unclass(x))
    invisible()
 }
+
