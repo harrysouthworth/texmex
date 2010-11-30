ec65cd2a4cdc5f07ede04b746afd885dc0de9062
harrys <harrys@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-11-30 15:35:55 +0000

    fixed summary.mexPrediction
    git-svn-id: https://texmex.googlecode.com/svn/pkg@110 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/summary.mexPrediction.R
+++ R/summary.mexPrediction.R
@@ -4,17 +4,32 @@ function( object, th, probs=c( .05, .5, .95 ), ... ){
 	if ( is.R() ) stdev <- function( x ) sqrt( var( x ) )
 	if ( missing( th ) ) th <- object$th
 
-	res <- t( sapply( object$replicates , function ( x ) apply( x, 2, mean ) ) )
-
-	sumfun <- function( x , probs){
-		c( mean=mean( x ), se=stdev( x ) , quantile( x, probs=probs ) )
+    if (!is.null(object$replicates)){
+    	res <- t( sapply( object$replicates , function ( x ) apply( x, 2, mean ) ) )
+    }
+    else {
+        res <- object$data$simulated
     }
 
+    # Create summary function
+	sumfun <- if (!is.null(object$replicates)){
+                function( x , probs){
+            		c( mean=mean( x ), se=stdev( x ) , quantile( x, probs=probs ) )
+                }
+              }
+              else {
+                function(x, probs){
+                    c(mean=mean(x), quantile(x, probs=probs))
+                }
+              }
+
 	ans <- apply( res, 2, sumfun, probs ) # Summary of expected values
-	dn <- paste( "E(", dimnames( object$replicates[[ 1 ]] )[[ 2 ]] ,"|", names( object$data$simulated )[[ 1 ]] , ">Q",100*object$pqu,")", sep="" )
+	dn <- paste( "E(", dimnames( object$data$simulated)[[ 2 ]] ,"|", names( object$data$simulated )[[ 1 ]] , ">Q",100*object$pqu,")", sep="" )
 	dimnames( ans )[[ 2 ]] <- dn
 
-	thres <- t( sapply( 1:( dim( object$replicates[[ 1 ]] )[[ 2 ]] ) ,
+    # Get the threshold exceedence probabilities
+    if (!is.null(object$replicates)){
+    	thres <- t( sapply( 1:( dim( object$replicates[[ 1 ]] )[[ 2 ]] ) ,
 						function( i, x, th ){
 							x <- sapply( x , function( x, i ) x[,i], i=i )
 							th <- th[ i ]
@@ -22,13 +37,19 @@ function( object, th, probs=c( .05, .5, .95 ), ... ){
 											mean( x > th ),
 										th = th )
 						}, x=object$replicates, th = th ) )
-	thres <- apply( thres, 1, mean )
-	thres <- matrix( thres, nrow=1 )
+	    thres <- apply( thres, 1, mean )
+    	thres <- matrix( thres, nrow=1 )
+    }
+    else {
+        thres <- sapply(1:dim(object$data$simulated)[[2]], function(i, x, th){ mean(x[,i] > th[i]) },
+                            x = object$data$simulated, th=th)
+        thres <- matrix(thres, nrow=1)
+    }
 
 	wn <- dimnames( object$data$simulated )[[ 2 ]][ 1 ]
 	wth <- paste( "Q", 100*object$pqu, sep = "" )
 #	dn <- paste( "P(", dimnames( object$replicates[[ 1 ]] )[[ 2 ]] , ">", th[ -1 ],"|", wn, ">", wth, ")", sep = "" )
-	dn <- paste( "P(", dimnames( object$replicates[[ 1 ]] )[[ 2 ]] , ">", th,"|", wn, ">", wth, ")", sep = "" )
+	dn <- paste( "P(", dimnames( object$data$simulated )[[ 2 ]] , ">", th,"|", wn, ">", wth, ")", sep = "" )
 
 	dimnames( thres ) <- list( "", dn )
 	
@@ -40,5 +61,23 @@ function( object, th, probs=c( .05, .5, .95 ), ... ){
 
 	oldClass( ans ) <- "summary.mexPrediction"
 	ans
+}
+
+`print.summary.mexPrediction` <-
+function( x, ... ){
+	print( x$call, ... )
+
+    if (x$B > 0){
+        cat( "\nResults from", x$B, "bootstrap runs.\n" )
+    }
+	cat( paste( "\nConditioned on ", x$which, " being above its ", 100*x$pqu, "th percentile.\n\n", sep = "" ) )
+	cat( "\nExpected values:\n\n" )
+	print( x$ans, ... )
+
+	cat( "\nConditional probability of threshold exceedance:\n\n" )
+
+	print( x$thres, ... )
+	
+	invisible()
 }
 
