9f8d01fbe3a3bc1cc31e963ee503734717c63001
harrys <harrys@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-12-01 10:22:44 +0000

    Deleted two files, replaced with testingFunctions.R
    git-svn-id: https://texmex.googlecode.com/svn/pkg@123 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/testingFunctions.R
+++ R/testingFunctions.R
@@ -1,3 +1,26 @@
+# NOT FOR END-USERS.
+# THESE ARE FUNCTIONS TAKEN FROM THE ismev AND evd PACKAGES AND ARE USED
+# SOLELY FOR TESTING texmex.
+# Date evd installed: 2010-12-1
+# Date ismev installed: 2010-12-1
+
+.evd.qgpd <-
+function (p, loc = 0, scale = 1, shape = 0, lower.tail = TRUE) 
+{
+    if (min(p, na.rm = TRUE) <= 0 || max(p, na.rm = TRUE) >= 
+        1) 
+        stop("`p' must contain probabilities in (0,1)")
+    if (min(scale) < 0) 
+        stop("invalid scale")
+    if (length(shape) != 1) 
+        stop("invalid shape")
+    if (lower.tail) 
+        p <- 1 - p
+    if (shape == 0) 
+        return(loc - scale * log(p))
+    else return(loc + scale * (p^(-shape) - 1)/shape)
+}
+
 .ismev.gpd.fit <-
 function (xdat, threshold, npy = 365, ydat = NULL, sigl = NULL, 
     shl = NULL, siglink = identity, shlink = identity, siginit = NULL, 
