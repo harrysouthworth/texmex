f05e1566b19094fedd11e4350466c367a05cc8ef
harrys <harrys@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-12-03 10:27:17 +0000

    tidying up deletion of coef.bootmex
    git-svn-id: https://texmex.googlecode.com/svn/pkg@158 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/print.bootmex.R
+++ R/print.bootmex.R
@@ -3,7 +3,6 @@ function( y , ... ){
 	print( y$call )
 	cat( paste( "\n", y$R, " bootstrap samples created.\n\n" , sep = "" ) )
 
-  co <- coef( y )
   eff <- attributes( co )$"Effective samples"
   cat( paste("Dependence structure estimation successful for", eff, "effective samples.\n" ) )
   cat( paste("Dependence structure bootstrap mean parameter estimates:\n" ))
