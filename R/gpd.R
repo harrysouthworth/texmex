5ed9f730a261dc260438ebeac78db8d2601b70a4
harrys <harrys@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-12-10 16:48:17 +0000

    added method arg to gpd, all tests pass :-)
    git-svn-id: https://texmex.googlecode.com/svn/pkg@182 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/gpd.R
+++ R/gpd.R
@@ -1,8 +1,55 @@
 gpd <- 
-function (y, data, th, qu, phi = ~1, xi = ~1, penalty = "gaussian", 
-    start=NULL,
-    priorParameters = NULL, maxit = 10000, trace = 0) {
+function (y, data, th, qu, phi = ~1, xi = ~1,
+          penalty = "gaussian", prior = "gaussian", method = "optimize",
+          start = NULL, priorParameters = NULL, maxit = 10000, trace=NULL,
+          iter = 10500, burn=500, thin = 1, jump.const, verbose=TRUE) {
+
     theCall <- match.call()
+
+    ##################### Sort out penalties, priors, methods...
+
+    if (!missing(penalty) & !missing(prior)){
+        stop("specify one or neither of penalty and prior, not both")
+    }
+    if (!missing(penalty)){
+        prior <- penalty
+    }
+
+    method <- casefold(method)
+    prior <- casefold(prior)
+
+    if (method %in% c("o", "opt", "optim", "optimize", "optimise")){
+        method <- "o"
+    }
+    else if (method %in% c("s", "sim", "simulate")){
+        method <- "s"
+    }
+    else {
+        stop("method should be either 'optimize' or 'simulate'")
+    }
+
+    if (method == "s" & prior != "gaussian"){
+        stop("only Gaussian prior can be used when simulating from the posterior")
+    }
+
+    ##################### Sort out trace
+    if (method == "o"){
+        if (!missing(trace)){
+            otrace <- trace
+        }
+        else {
+            otrace <- 0
+        }
+    }
+    else{
+       otrace <- 0
+       if (missing(trace)){
+           trace <- 1000
+       }
+    }
+              
+    ############################## Construct data to use...
+
     if (!missing(data)) {
       y <- deparse(substitute(y))
     }
@@ -39,13 +86,17 @@ function (y, data, th, qu, phi = ~1, xi = ~1, penalty = "gaussian",
     if (length(y) == 0) {
         stop("No observations over threshold")
     }
-    if (casefold(penalty) %in% c("quadratic", "gaussian")) {
+
+
+    ###################### Check and sort out prior parameters...
+
+    if (prior %in% c("quadratic", "gaussian")) {
         if (is.null(priorParameters)) {
             priorParameters <- list(rep(0, ncol(X.phi) + ncol(X.xi)), 
                 diag(rep(10^4, ncol(X.phi) + ncol(X.xi))))
         }
     }
-    else if (casefold(penalty) %in% c("lasso", "l1", "laplace")) {
+    else if (prior %in% c("lasso", "l1", "laplace")) {
         if (is.null(priorParameters)) {
             priorParameters <- list(rep(0, ncol(X.phi) + ncol(X.xi)), 
                 diag(rep(10^(-4), ncol(X.phi) + ncol(X.xi))))
@@ -61,7 +112,9 @@ function (y, data, th, qu, phi = ~1, xi = ~1, penalty = "gaussian",
             warning("some off-diagonal elements of the covariance are non-zero. Only the diagonal is used in penalization")
         }
     }
-    if (!is.null(priorParameters)) {
+
+    #### If priorParameters given but of wrong dimension, kill
+    if (!missing(priorParameters)) {
         dimp <- ncol(X.phi) + ncol(X.xi)
         if (length(priorParameters[[1]]) != dimp) {
             stop("wrong number of parameters in prior (doesn't match phi and xi formulas)")
@@ -71,39 +124,136 @@ function (y, data, th, qu, phi = ~1, xi = ~1, penalty = "gaussian",
         }
     }
 
+    ################################## Do the optimization....
 
-    o <- gpd.fit(y=y, th=th, X.phi=X.phi, X.xi=X.xi, penalty=penalty,
-                 priorParameters = priorParameters, maxit = maxit, trace = trace)
+    o <- gpd.fit(y=y, th=th, X.phi=X.phi, X.xi=X.xi, penalty=prior,
+                 priorParameters = priorParameters, maxit = maxit, trace = otrace)
 
 
     if (o$convergence != 0) {
         warning("Non-convergence in gpd")
     }
 
-    o$cov <- solve(o$hessian)
-    o$hessian <- NULL
-    o$se <- sqrt(diag(o$cov))
-    o$threshold <- th
-    o$penalty <- penalty
-    o$coefficients <- o$par
-    names(o$coefficients) <- c(paste("phi:", colnames(X.phi)), 
-        paste("xi:", colnames(X.xi)))
-    o$par <- NULL
-    o$rate <- rate
-    o$call <- theCall
-    o$y <- y
-    o$X.phi <- X.phi
-    o$X.xi <- X.xi
-    if (missing(data)) {
-        data <- NULL
-    }
-    o$data <- data
+    ################################## If method = "optimize", construct object and return...
 
-    o$loglik <- -o$value
-    o$value <- NULL
-    o$counts <- NULL
-    oldClass(o) <- "gpd"
-    o
+    if (method == "o"){
+        o$cov <- solve(o$hessian)
+        o$hessian <- NULL
+        o$se <- sqrt(diag(o$cov))
+        o$threshold <- th
+        o$penalty <- prior
+        o$coefficients <- o$par
+        names(o$coefficients) <- c(paste("phi:", colnames(X.phi)), 
+                                   paste("xi:", colnames(X.xi)))
+        o$par <- NULL
+        o$rate <- rate
+        o$call <- theCall
+        o$y <- y
+        o$X.phi <- X.phi
+        o$X.xi <- X.xi
+        if (missing(data)) {
+            data <- NULL
+        }
+        o$data <- data
+
+        o$loglik <- -o$value
+        o$value <- NULL
+        o$counts <- NULL
+        oldClass(o) <- "gpd"
+        o
+    } # Close if (method == "o"
+
+    ################################# Simulate from posteriors....
+
+    else { # Method is "simulate"...
+   
+        if (missing(jump.const)){
+            jump.const <- (2.4/sqrt(ncol(X.phi) + ncol(X.xi)))^2
+        }
+
+        u <- th
+	
+        # Next line seems redundant, but leave in case want to allow other priors in future
+        prior <- dmvnorm
+
+        gpdlik <- # Positive loglikelihood
+        function(param, data, X.phi, X.xi){
+            phi <- colSums(param[1:ncol(X.phi)] * t(X.phi))
+            xi <- colSums(param[(ncol(X.phi) + 1):(ncol(X.phi) + ncol(X.xi))] * t(X.xi))
+
+            n <- length(data)
+            data <- xi * data / exp(phi) + 1
+            if(any(data <= 0)){ #| ( xi <= -.50 ) ) -(10^10)
+                -(10^10)
+            }
+            else  {
+                - sum(phi) - sum(log(data) * (1/xi + 1))
+            }
+        } # Close gpdlik <- function
+
+
+
+        # Need to check for convergence failure here. Otherwise, end up simulating
+        # proposals from distribution with zero variance in 1 dimension.
+        checkNA <- any(is.na(sqrt(diag(o$cov))))
+        if ( checkNA ){
+            stop( "MAP estimates have not converged. Cannot proceed. Try a different prior" )
+        }
+
+        res <- matrix( ncol=ncol(X.phi) + ncol(X.xi), nrow=iter )
+
+        if (missing(start)) {
+            res[1, ] <- o$par
+        }
+	    else { 
+            res[1, ] <- start
+        }
+
+        if (!exists(".Random.seed")){ runif(1)  }
+        seed <- .Random.seed # Retain and add to output
+
+
+        cov <- solve(o$hessian)
+        ######################## Run the Metropolis algorithm...
+        for(i in 2:iter){
+            if( verbose){
+                if( i %% trace == 0 ) cat(i, " steps taken\n" )
+            }
+        
+            if ( exists("is.R") && is.function(is.R) && is.R() ){
+                prop <- c(rmvnorm( 1 , mean = o$par, sigma = cov * jump.const ))
+            }
+            else {
+                prop <- c(rmvnorm( 1 , mean = o$par, cov = cov * jump.const ))
+            }
+            bottom <- prior(res[i - 1,], priorParameters[[1]], priorParameters[[2]], log=TRUE ) +
+                      gpdlik(res[ i - 1 , ] , y-u, X.phi, X.xi)
+            top <- prior(prop, priorParameters[[1]], priorParameters[[2]], log=TRUE) +
+                   gpdlik(prop, y-u, X.phi, X.xi)
+            r <- exp(top - bottom)
+            res[ i , ] <- if (runif(1) < r ){ prop }
+                          else{ res[i -1, ] }
+        } # Close for(i in 2:iter
+
+        acc <- length(unique(res[,1])) / iter
+        if (acc < .1){ warning("Acceptance rate in Metropolis algorithm is low.") }
+        if (trace < iter){
+            if(verbose) {
+                cat("Acceptance rate:", round( acc , 3 ) , "\n")
+            }
+        }
+
+        res <- list(call=theCall, threshold=u , map = o,
+                    burn = burn, thin = thin, 
+                    chains=res ,
+                    X.phi = X.phi, X.xi = X.xi,
+					acceptance=acc, seed=seed 
+                    )
+        oldClass(res) <- "bgpd"
+        res <- thinAndBurn(res)
+        res
+    } # Close else 
+
 }
 
 test(gpd) <- function(){
@@ -323,5 +473,129 @@ test(gpd) <- function(){
 
   checkTrue(abs(4 - coef(mod9)[2])  > abs(4 - coef(mod10)[2]),msg="gpd: with covariates, phi drawn to 4")
   checkTrue(abs(4 - coef(mod10)[2]) > abs(4 - coef(mod11)[2]),msg="gpd: with covariates, phi drawn to 4")
+
+
+
+
+
+
+
+
+  postSum <- function(x){
+    t(apply(x$param, 2, function(o){ c(mean=mean(o), se=sd(o)) }))
+  } 
+  
+#************************************************************* 
+# 4.1. Test reproducibility
+
+  set.seed(20101110)
+  save.seed <- .Random.seed
+
+  set.seed(save.seed)
+  bmod <- gpd(ALT_M, data=liver, th=quantile(liver$ALT_M, .7), iter=1000,verbose=FALSE, method="sim")
+  
+  set.seed(save.seed)
+  bmod2 <- gpd(ALT_M, data=liver, th=quantile(liver$ALT_M, .7), iter=1000,verbose=FALSE, method="sim")
+  
+  checkEqualsNumeric(bmod$param,bmod2$param,msg="gpd: test simulation reproducibility 1")
+
+  set.seed(bmod$seed)
+  bmod3 <- gpd(ALT_M, data=liver, th=quantile(liver$ALT_M, .7), iter=1000,verbose=FALSE, method="sim")
+  checkEqualsNumeric(bmod$param, bmod3$param,msg="gpd: test simulation reproducibility 2")
+
+#*************************************************************  
+# 4.2. Logical test of burn-in
+
+  checkEqualsNumeric(nrow(bmod$chains) - bmod$burn, nrow(bmod$param),msg="gpd: Logical test of burn-in 1")
+
+  iter <- sample(500:1000,1)
+  burn <- sample(50,1)
+  bmod2 <- gpd(ALT_M, data=liver, th=quantile(liver$ALT_M, .7),
+                iter=iter, burn=burn, verbose=FALSE, method="sim")
+
+  checkEqualsNumeric(iter-burn, nrow(bmod2$param),msg="gpd: Logical test of burn-in 2")
+
+#*************************************************************
+# 4.3. Logical test of thinning
+
+  thin <- 0.5
+  iter <- 1000
+  bmod <- gpd(ALT_M, data=liver, th=quantile(liver$ALT_M, .7),
+               iter=iter, thin = thin,verbose=FALSE, method="sim")
+
+  checkEqualsNumeric((nrow(bmod$chains) - bmod$burn) * thin, nrow(bmod$param),msg="gpd: Logical test of thinning 1")
+
+  thin <- 2
+  iter <- 1000
+  bmod <- gpd(ALT_M, data=liver, th=quantile(liver$ALT_M, .7),
+               iter=iter, thin = thin, verbose=FALSE, method="sim")
+
+  checkEqualsNumeric((nrow(bmod$chains) - bmod$burn) / thin, nrow(bmod$param),msg="gpd: Logical test of thinning 1")
+
+#*************************************************************  
+## Checks of gpd simulation. Centres of distributions and standard deviations 
+## should be similar to those coming out of gpd with the same penalty.
+## Posterior means are not the same as MAPs and SEs from Hessian are
+## approximations, so allow some tolerance.
+
+  tol <- 0.01
+  tol.se <- 0.2
+# 4.4. Compare MAP and posterior summaries for simple model
+
+  mod <- gpd(ALT_M, data=liver, qu=.7)
+  bmod <- gpd(ALT_M, data=liver, th=quantile(liver$ALT_M, .7),verbose=FALSE, method="sim")
+  
+  checkEqualsNumeric(coef(mod), postSum(bmod)[,1], tolerance=tol,msg="gpd: Compare MAP and posterior summaries for simple model - point ests")
+  checkEqualsNumeric(mod$se, postSum(bmod)[,2], tolerance=tol.se,msg="gpd: Compare MAP and posterior summaries for simple model - std errs")
+
+#*************************************************************
+# 4.5. Covariates in phi
+  tol <- 0.01
+  tol.se <- 0.2
+
+  require(MASS,quiet=TRUE) # Use MASS because is ships with R and has no dependencies
+              # that don't ship with R.
+  liver <- liver
+  liver$ndose <- as.numeric(liver$dose)
+
+# function rlm : Fit a linear model by robust regression using an M estimator:
+
+  rmod <- rlm(log(ALT_M) ~ log(ALT_B) + dose, data=liver) 
+  liver$resids <- resid(rmod)
+
+  mod <- gpd(resids, data=liver, qu=.7, phi = ~ ndose)
+
+  bmod <- gpd(resids, data=liver, th=quantile(liver$resids, .7),
+               phi = ~ ndose,verbose=FALSE, method="sim")
+              
+  checkEqualsNumeric(coef(mod), postSum(bmod)[,1], tolerance=tol,msg="gpd: Compare MAP and posterior summaries for Covariates in phi - point ests")
+  checkEqualsNumeric(mod$se, postSum(bmod)[,2], tolerance=tol.se,msg="gpd: Compare MAP and posterior summaries for Covariates in phi - std errs")
+
+#*************************************************************
+# 4.6. Covariates in xi
+  tol <- 0.02
+  tol.se <- 0.2
+
+  mod <- gpd(resids, data=liver, qu=.7, xi = ~ ndose)
+  bmod <- gpd(resids, data=liver, th=quantile(liver$resids, .7),
+               xi = ~ ndose,verbose=FALSE, method="sim")
+
+  checkEqualsNumeric(coef(mod), postSum(bmod)[,1], tolerance=tol,msg="gpd: Compare MAP and posterior summaries for Covariates in xi - point ests")
+  checkEqualsNumeric(mod$se, postSum(bmod)[,2], tolerance=tol.se,msg="gpd: Compare MAP and posterior summaries for Covariates in xi - std errs")  
+
+#*************************************************************
+# 4.7. Covariates in xi and phi
+  tol <- 0.02
+  tol.se <- 0.3
+
+  mod <- gpd(resids, data=liver, qu=.7,
+             xi = ~ ndose, 
+             phi = ~ ndose)
+  bmod <- gpd(resids, data=liver, th=quantile(liver$resids, .7),
+               xi = ~ ndose, 
+               phi = ~ ndose,verbose=FALSE, method="sim")
+               
+  checkEqualsNumeric(coef(mod), postSum(bmod)[,1], tolerance=tol,msg="gpd: Compare MAP and posterior summaries for Covariates in phi and xi - point ests")
+  checkEqualsNumeric(mod$se, postSum(bmod)[,2], tolerance=tol.se,msg="gpd: Compare MAP and posterior summaries for Covariates in phi and xi - std errs")  
 }
 
--- R/gpd.fit.R
+++ R/gpd.fit.R
@@ -2,7 +2,7 @@ gpd.fit <- function(y, th, X.phi, X.xi, penalty="none", start=NULL,
         priorParameters = NULL, maxit = 10000, trace = 0) {
 
     gpd.lik <- function(par, y, th, X.phi, X.xi, penalty = "none", 
-        priorParameters = NULL, maxit = maxit, trace = trace) {
+        priorParameters = NULL) {
         keepsc <- par[1:ncol(X.phi)]
         keepxi <- par[-(1:ncol(X.phi))]
         sc <- colSums(par[1:ncol(X.phi)] * t(X.phi))
@@ -10,17 +10,21 @@ gpd.fit <- function(y, th, X.phi, X.xi, penalty="none", start=NULL,
             t(X.xi))
         y <- (y - th)/exp(sc)
         y <- 1 + xi * y
-        if (min(y) <= 0) 
+        if (min(y) <= 0) {
             l <- 10^6
-        else l <- sum(sc) + sum(log(y) * (1/xi + 1))
-        if (casefold(penalty) == "none") 
+        {
+        else {
+            l <- sum(sc) + sum(log(y) * (1/xi + 1))
+        }
+        if (penalty == "none") {
             l <- l
-        else if (casefold(penalty) %in% c("quadratic", "gaussian")) {
+        }
+        else if (penalty %in% c("quadratic", "gaussian")) {
             p <- mahalanobis(matrix(c(keepsc, keepxi), nrow = 1), 
                 center = priorParameters[[1]], cov = priorParameters[[2]])
             l <- l + p
         }
-        else if (casefold(penalty) %in% c("lasso", "l1", "laplace")) {
+        else if (penalty %in% c("lasso", "l1", "laplace")) {
             p <- sum(abs(c(keepsc, keepxi) - priorParameters[[1]]) * 
                 diag(priorParameters[[2]]))
             l <- l + p
