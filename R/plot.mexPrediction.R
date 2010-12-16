9f588babdea89253dcc9bca52f2ae83ce828f24c
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-12-16 13:13:57 +0000

    plot.mexPrediction.R: added curve joining equal quantiles to plot as in Fig 6 Heffernan and Tawn; fixed bug adding vertical line to plot with prediction threshold; added test attribute to this function.
    git-svn-id: https://texmex.googlecode.com/svn/pkg@218 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/plot.mexPrediction.R
+++ R/plot.mexPrediction.R
@@ -12,7 +12,9 @@ function( x, pch=c( 1, 3 ), col=c( 2, 8 ), cex=c( 1, 1 ), ask = TRUE, ... ){
     }
 
 	xdat <- x$data$real[, 1 ]
-	
+  upts <- seq(from =0.001,to=1-0.0001,len=100)
+  xpts <- revGumbel(upts,data=x$data$real[, 1 ], qu = mean(x$data$real[,1] < x$mth[1]), th=x$mth[1],sigma = x$gpd.coef[3,1], xi = x$gpd.coef[4,1])
+
 	for( i in 2:( dim( x$data$real )[[ 2 ]] ) ){
 		ydat <- x$data$real[, i ]
 		xlimits <- range( xdat , x$data$simulated[ , 1 ] )
@@ -21,14 +23,35 @@ function( x, pch=c( 1, 3 ), col=c( 2, 8 ), cex=c( 1, 1 ), ask = TRUE, ... ){
 		plot( xdat , ydat , xlim=xlimits , ylim=ylimits,
 			  xlab = names( x$data$simulated )[ 1 ],
 			  ylab = names( x$data$simulated )[ i ],
-			  type = "n"
+			  type = "n",...
 			 )
-		abline( v = x$data$pth, lty=4 )
 		points( x$data$simulated[, 1 ] , x$data$simulated[, i ] ,
 				col=col[ 2 ] , pch=pch[ 2 ], cex=cex[ 2 ] )
 		points( xdat, ydat , pch=pch[ 1 ], col=col[ 1 ], cex= cex[ 1 ] )
+		abline( v = x$data$pth, lty=2, col="blue" )   
+    ypts <- revGumbel(upts,data=x$data$real[, i ], qu = mean(x$data$real[,i] < x$mth[i]), th=x$mth[i],sigma = x$gpd.coef[3,i], xi = x$gpd.coef[4,i])
+    lines(xpts,ypts,col="blue")
 	}
 	
 	invisible()
 }
 
+test(plot.predict.mex) <- function(){
+# check reproduce Figure 6 in Heffernan and Tawn
+  w <- migpd(winter,mqu=0.7,penalty="none")
+  noMod <- bootmex(w, wh="NO", dqu=.7)  
+  noPred <- predict(noMod)
+  par(mfcol=c(2,2))
+  res <- plot(noPred,main="Fig. 6 Heffernan and Tawn (2004)")
+  checkEquals(res, NULL, msg="plot.predict.mex: correct execution")
+  
+# check for 2-d data
+  R <- 20
+  nsim <- 100
+  wavesurge.fit <- migpd(wavesurge,mq=.7)
+  wavesurge.boot <- bootmex(wavesurge.fit,which=1,R=R)
+  wavesurge.pred <- predict(wavesurge.boot,nsim=nsim)
+  par(mfrow=c(1,1))
+  res <- plot(wavesurge.pred)
+  checkEquals(res, NULL, msg="plot.predict.mex: correct execution")
+}
