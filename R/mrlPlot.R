8e314570acd7e48aa7beb4b58c4543c8f4657d09
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-12-16 13:09:46 +0000

    mrlPlot.R : added test attribute to this function, also changed name from "u" to "threshold" for consistency with gpdRangeFit.
    git-svn-id: https://texmex.googlecode.com/svn/pkg@214 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/mrlPlot.R
+++ R/mrlPlot.R
@@ -4,18 +4,23 @@ function (data, umin = min(data), umax = max(data) - 0.1, conf = 0.95,
 {
     data <- c(data)
     x <- xu <- xl <- numeric(nint)
-    u <- seq(umin, umax, length = nint)
+    Threshold <- seq(umin, umax, length = nint)
     for (i in 1:nint) {
-        data <- data[data > u[i]]
-        x[i] <- mean(data - u[i])
+        data <- data[data > Threshold[i]]
+        x[i] <- mean(data - Threshold[i])
         sdev <- sqrt(var(data))
         n <- length(data)
         xu[i] <- x[i] + (qnorm((1 + conf)/2) * sdev)/sqrt(n)
         xl[i] <- x[i] - (qnorm((1 + conf)/2) * sdev)/sqrt(n)
     }
-    plot(u, x, type = "l", ylab = "Mean Excess", 
+    plot(Threshold, x, type = "l", ylab = "Mean Excess", 
         ylim = c(min(xl[!is.na(xl)]), max(xu[!is.na(xu)])), ...)
-    lines(u[!is.na(xl)], xl[!is.na(xl)], lty = 2)
-    lines(u[!is.na(xu)], xu[!is.na(xu)], lty = 2)
+    lines(Threshold[!is.na(xl)], xl[!is.na(xl)], lty = 2)
+    lines(Threshold[!is.na(xu)], xu[!is.na(xu)], lty = 2)
 }
 
+test(mrlPlot) <- function(){
+  par(mfrow=c(1,1))
+  res <- mrlPlot(rain, main="Figure 4.1 of Coles (2001)")
+  checkEquals(res,NULL,msg="mrlPlot: check execution")
+}
