524f3c59cbcfedec1c757ab5f214e60272adbaa6
harrys <harrys@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2010-12-20 13:17:33 +0000

    updated validate.texmex.R
    git-svn-id: https://texmex.googlecode.com/svn/pkg@236 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/validate.texmex.R
+++ R/validate.texmex.R
@@ -1,4 +1,4 @@
-validate.texmex <- function(testDir){
+validate.texmex <- function(){
    check <- "package:RUnit" %in% search()
    if (!check){
       check <- try(library(RUnit))
@@ -6,11 +6,21 @@ validate.texmex <- function(testDir){
           stop("You need to attach the RUnit package to validate texmex")
       }
    }
-   if (missing(testDir)){
-       testDir <- paste(.path.package("texmex"), "R", sep = "/")
+
+   # Create a temporary directory to store tests scripts
+   tempDir <- paste(sample(c(letters, LETTERS, 0:9), size=20), collapse="")
+   tempDir <- paste(".", tempDir, sep = "")
+   on.exit(system(paste("rm -r", tempDir)))
+   system(paste("mkdir", tempDir))
+
+   # Get tests and dump to temporary directory
+   wh <- (1:length(search()))[search() == "package:texmex"]
+   tests <- objects(wh)[substring(objects(wh), 1, 5) == "test."]
+   for (i in 1:length(tests)){
+      dump(tests[i], file=paste(tempDir, "/", tests[i], ".R", sep = ""))
    }
-   
-   res <- defineTestSuite("texmex", dirs=testDir,
+
+   res <- defineTestSuite("texmex", dirs=tempDir,
                           testFuncRegexp="^test.+",
                           testFileRegexp="*.R")
    cat("Running over 100 tests, including MCMC and bootstrap implementations.\nThis will take some time...\n\n")
