c604d582804b213ffa1e4e83485be56df2e5145a
harrys <harrys@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2011-03-15 19:30:41 +0000

    validate.texmex ought to work in Win and Lin now
    git-svn-id: https://texmex.googlecode.com/svn/pkg@260 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/validate.texmex.R
+++ R/validate.texmex.R
@@ -1,32 +1,22 @@
-validate.texmex <- function(){
+validate.texmex <- function () {
    check <- "package:RUnit" %in% search()
-   if (!check){
-      check <- try(library(RUnit))
-      if (class(check) == "try-error"){
-          stop("You need to attach the RUnit package to validate texmex")
-      }
+   if (!check) {
+       check <- try(library(RUnit))
+       if (class(check) == "try-error") {
+           stop("You need to attach the RUnit package to validate texmex")
+       }
    }
 
-   # Create a temporary directory to store tests scripts
-   tempDir <- paste(sample(c(letters, LETTERS, 0:9), size=20), collapse="")
-   tempDir <- paste(".", tempDir, sep = "")
-   on.exit(system(paste("rm -r", tempDir)))
-   system(paste("mkdir", tempDir))
-
-   # Get tests and dump to temporary directory
    wh <- (1:length(search()))[search() == "package:texmex"]
    tests <- objects(wh)[substring(objects(wh), 1, 5) == "test."]
-   for (i in 1:length(tests)){
-      dump(tests[i], file=paste(tempDir, "/", tests[i], ".R", sep = ""))
+
+   for (i in 1:length(tests)) {
+       str <- paste(tempdir(), "\\", tests[i], ".R", sep = "")
+       dump(tests[i], file = str)
    }
-
-   res <- defineTestSuite("texmex", dirs=tempDir,
-                          testFuncRegexp="^test.+",
-                          testFileRegexp="*.R")
+   res <- defineTestSuite("texmex", dirs = tempdir(), testFuncRegexp = "^test.+", testFileRegexp = "*.R")
    cat("Running over 100 tests, including MCMC and bootstrap implementations.\nThis will take some time...\n\n")
-
    res <- runTestSuite(res)
-
    res
 }
 
