900a51a7a427610da07ad503bc1d30be822db40a
harrys <harrys@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2011-05-03 13:54:59 +0000

    fixed annoyance with print.mnt.mexDependence
    git-svn-id: https://texmex.googlecode.com/svn/pkg@274 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/print.mexDependence.R
+++ R/print.mexDependence.R
@@ -3,8 +3,8 @@ function(x, ...){
 	# print(x$call, ...)
     cat("Conditioning on ", x$conditioningVariable, " variable.\n", sep="")
 	names(x$dqu) <- dimnames(x$parameters)[[2]]
-	cat("\nThresholding quantiles for transformed data:\n")
-	print(x$dqu, ...)
+	cat("\nThresholding quantiles for transformed data:\n", x$dqu, "\n", sep="")
+	#print(x$dqu, ...)
 	cat("\nDependence structure parameter estimates:\n")
 	if (any(abs(x$coefficients)[3:4, ] > 10^(-6))){
 		print(x$coefficients, ...)
