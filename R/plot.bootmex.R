aede13613cfcc995737eb99a2489e2ebb9c8f7f6
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2011-05-05 12:42:54 +0000

    plot.bootmex.R: updated function and testing to accommodate choice of margins in dependence fitting.
    git-svn-id: https://texmex.googlecode.com/svn/pkg@293 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/plot.bootmex.R
+++ R/plot.bootmex.R
@@ -10,6 +10,7 @@ function( x , plots = "gpd", main="", ... ){
 	pointEst <- x$simpleDep
   
   condVar <- names(x$simpleMar$data)[x$which]
+  margins <- x$margins
 	x <- x$boot
 	co <- unlist( lapply( x , function( z, wh ) z[[ wh ]], wh=which ) )
 	co <- array(co, dim = c( d2[1] , d2[2] , length(co) / prod(d2)))
@@ -44,8 +45,10 @@ function( x , plots = "gpd", main="", ... ){
       offset <- (X-1) * 4
       plot(lco[[offset + 1]],lco[[offset + 2]],xlab=labs[offset + 1],ylab=labs[offset + 2],main=main, ...)
       points(pointEst[1,X],pointEst[2,X],pch="@",col="red")
-      plot(lco[[offset + 3]],lco[[offset + 4]],xlab=labs[offset + 3],ylab=labs[offset + 4],main=main, ...)
-      points(pointEst[3,X],pointEst[4,X],pch="@",col="red")
+      if( margins == "gumbel"){
+        plot(lco[[offset + 3]],lco[[offset + 4]],xlab=labs[offset + 3],ylab=labs[offset + 4],main=main, ...)
+        points(pointEst[3,X],pointEst[4,X],pch="@",col="red")
+      }
     }
     lapply(1:d2[2], fun, z=lco,label=labs, ...)
   }
@@ -60,7 +63,7 @@ test.plot.bootmex <- function(){
   wavesurge.boot <- bootmex(wavesurge.fit,which=1,R=50)
   par(mfrow=c(3,2),pty="m")
   check1 <- plot(wavesurge.boot,main="Marginal parameters\nWave surge data of Coles 2001")
-  check2 <- plot(wavesurge.boot,plots="dep",main="Dependence parameters\nWave surge data of Coles 2001")
+  check2 <- plot(wavesurge.boot,plots="dep",main="Dependence parameters\nWave surge data of Coles 2001\nLaplace margins")
   
 # 5-d air pollution data
 
@@ -69,16 +72,16 @@ test.plot.bootmex <- function(){
   
   Qu <- 0.7
   R <- 50
-  Sboot.O3 <- bootmex(smarmod, which=1, dqu=Qu, R=R)
-  Wboot.O3 <- bootmex(wmarmod, which=1, dqu=Qu, R=R)
-  Sboot.NO2 <- bootmex(smarmod, which=2, dqu=Qu, R=R)
-  Wboot.NO2 <- bootmex(wmarmod, which=2, dqu=Qu, R=R)
-  Sboot.NO <- bootmex(smarmod, which=3, dqu=Qu, R=R)
-  Wboot.NO <- bootmex(wmarmod, which=3, dqu=Qu, R=R)
-  Sboot.SO2 <- bootmex(smarmod, which=4, dqu=Qu, R=R)
-  Wboot.SO2 <- bootmex(wmarmod, which=4, dqu=Qu, R=R)
-  Sboot.PM10 <- bootmex(smarmod, which=5, dqu=Qu, R=R)
-  Wboot.PM10 <- bootmex(wmarmod, which=5, dqu=Qu, R=R)
+  Sboot.O3 <- bootmex(smarmod, which=1, dqu=Qu, R=R, margins="gumbel")
+  Wboot.O3 <- bootmex(wmarmod, which=1, dqu=Qu, R=R, margins="gumbel")
+  Sboot.NO2 <- bootmex(smarmod, which=2, dqu=Qu, R=R, margins="gumbel")
+  Wboot.NO2 <- bootmex(wmarmod, which=2, dqu=Qu, R=R, margins="gumbel")
+  Sboot.NO <- bootmex(smarmod, which=3, dqu=Qu, R=R, margins="gumbel")
+  Wboot.NO <- bootmex(wmarmod, which=3, dqu=Qu, R=R, margins="gumbel")
+  Sboot.SO2 <- bootmex(smarmod, which=4, dqu=Qu, R=R, margins="gumbel")
+  Wboot.SO2 <- bootmex(wmarmod, which=4, dqu=Qu, R=R, margins="gumbel")
+  Sboot.PM10 <- bootmex(smarmod, which=5, dqu=Qu, R=R, margins="gumbel")
+  Wboot.PM10 <- bootmex(wmarmod, which=5, dqu=Qu, R=R, margins="gumbel")
 
   par(mfrow=c(4,2))
   check3 <- plot(Sboot.O3,plots="dep",main="Summer air pollution data\nFig.5 Heffernan & Tawn 2004",xlim=c(0,1),ylim=c(-1,1))
