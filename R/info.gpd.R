e515efeac8a7792922691ba1623c41a3ffdca399
harrys <harrys@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2011-05-11 15:44:48 +0000

    info.gpd returns zeroes if info is singular
    git-svn-id: https://texmex.googlecode.com/svn/pkg@308 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/info.gpd.R
+++ R/info.gpd.R
@@ -78,11 +78,13 @@ function(o, method="observed"){
 	}
 
 	i <- rbind( cbind(Is, Iks), cbind(t(Iks), Ik))
-	solve(i - p)
+	res <- try(solve(i - p))
+	if (class(res) == "try-error"){ res <- matrix(0, ncol=ns+nk, nrow=ns+nk) }
+	res
 }
 
 test.info.gpd <- function(){
-	lmod <- gpd(r, data=liver, qu=.5, xi=~I(240*as.numeric(dose)), cov="numeric")
+	lmod <- gpd(log(ALT.M) / log(ALT.B), data=liver, qu=.5, xi=~I(240*as.numeric(dose)), cov="numeric")
 	checkTrue(all(sqrt(diag(info.gpd(lmod))) > 0), msg="info.gpd: SDs positive")
 
 	# Check equality to numerical approximation in big samples
