ffc3ec2018422b7fdc6c00c0b424b1b54d93a0d7
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2011-05-12 10:58:23 +0000

    copulas.R : suppressing error messages in testing exceptions.
    git-svn-id: https://texmex.googlecode.com/svn/pkg@315 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/copulas.R
+++ R/copulas.R
@@ -81,6 +81,10 @@ test.copula <- function(){
 
   checkEqualsNumeric(d2,copula(u2)$copula,msg="copula: 2 dimensional")
   checkEqualsNumeric(d3,copula(u3)$copula,msg="copula: 3 dimensional")
+  
+  o <- options()
+  options(show.error.messages=FALSE)
   checkException(copula(TRUE),msg="copula: exception")
   checkException(copula("text"),msg="copula: exception")
+  options(op)
 }
