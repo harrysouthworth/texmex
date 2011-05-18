a7f84ae30e9c6c3ffc8eb187f924ddc573524fbc
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2011-05-18 10:21:49 +0000

    print.mexDependence.R : if constrain=TRUE in dependence estimation then print message giving value of v for constraint.
    git-svn-id: https://texmex.googlecode.com/svn/pkg@326 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/print.mexDependence.R
+++ R/print.mexDependence.R
@@ -1,16 +1,19 @@
 `print.mexDependence` <-
 function(x, ...){
-	# print(x$call, ...)
-    cat("Conditioning on ", x$conditioningVariable, " variable.\n", sep="")
+  cat("Conditioning on ", x$conditioningVariable, " variable.\n", sep="")
 	names(x$dqu) <- dimnames(x$parameters)[[2]]
+
 	cat("\nThresholding quantiles for transformed data:\n", x$dqu, "\n", sep="")
-	#print(x$dqu, ...)
+
   cat("\n",x$margins," margins used for Dependence estimation\n", sep="")
+  if(x$dependence$constrain){
+    cat("Constrained estimation of dependence parameters using v =",x$dependence$v,"\n")
+  }
+
 	cat("\nDependence structure parameter estimates:\n")
 	if (any(abs(x$coefficients)[3:4, ] > 10^(-6))){
 		print(x$coefficients[1:4,], ...)
-	}
-	else {
+	} else {
 		print(x$coefficients[1:2, ], ...)
 	}
 	invisible()
