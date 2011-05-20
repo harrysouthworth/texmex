2e211e561a9190b79c4e324fb2ba1ab8d03919b7
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2011-05-20 09:10:25 +0000

    print.mexDependence.R : added printing of likelihood value at max.
    git-svn-id: https://texmex.googlecode.com/svn/pkg@341 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/print.mexDependence.R
+++ R/print.mexDependence.R
@@ -5,11 +5,13 @@ function(x, ...){
 
 	cat("\nThresholding quantiles for transformed data:\n", x$dqu, "\n", sep="")
 
-  cat("\n",x$margins," margins used for Dependence estimation\n", sep="")
+  cat("\nUsing ",x$margins," margins for Dependence estimation\n", sep="")
   if(x$constrain){
-    cat("Constrained estimation of dependence parameters using v =",x$v,"\n")
+    cat("\nConstrained estimation of dependence parameters using v =",x$v,"\n")
   }
 
+  cat("\n Log-likelihood =",x$loglik,"\n")
+  
 	cat("\nDependence structure parameter estimates:\n")
 	if (any(abs(x$coefficients)[3:4, ] > 10^(-6))){
 		print(x$coefficients[1:4,], ...)
