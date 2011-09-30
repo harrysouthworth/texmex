22c392343f99783b50f1c0ee372a476c1d59fc9c
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2011-09-30 11:21:13 +0000

    qgpd2.R : no functional change - changed notation to be clearer and added comment.
    git-svn-id: https://texmex.googlecode.com/svn/pkg@371 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/qgpd2.R
+++ R/qgpd2.R
@@ -1,4 +1,4 @@
 `qgpd2` <-
-function(p , sigma = 1 , xi = 1 , u = 0, la = 1 )
-	u + ( sigma * ( ( p * la )^( xi ) - 1)) / xi
+function(N , sigma = 1 , xi = 1 , u = 0, la = 1 ) # returns N observation return level
+	u + ( sigma * ( ( N * la )^( xi ) - 1)) / xi
 
