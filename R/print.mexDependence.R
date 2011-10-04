1c49b178d58f093914db67954a36598315e4d728
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2011-10-04 09:36:19 +0000

    print.mexDependence.R : tidied up presentation of output for greater legibility.
    git-svn-id: https://texmex.googlecode.com/svn/pkg@380 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/print.mexDependence.R
+++ R/print.mexDependence.R
@@ -1,16 +1,16 @@
 `print.mexDependence` <-
 function(x, ...){
-  cat("Conditioning on ", x$conditioningVariable, " variable.\n", sep="")
+  cat("Conditioning on ", x$conditioningVariable, " variable.", sep="")
 	names(x$dqu) <- dimnames(x$parameters)[[2]]
 
-	cat("\nThresholding quantiles for transformed data:\n", x$dqu, "\n", sep="")
+	cat("\nThresholding quantiles for transformed data: dqu = ", x$dqu, sep="")
 
-  cat("\nUsing ",x$margins," margins for Dependence estimation\n", sep="")
+  cat("\nUsing ",x$margins," margins for dependence estimation.", sep="")
   if(x$constrain){
-    cat("\nConstrained estimation of dependence parameters using v =",x$v,"\n")
+    cat("\nConstrained estimation of dependence parameters using v =",x$v,".")
   }
 
-  cat("\n Log-likelihood =",x$loglik,"\n")
+  cat("\nLog-likelihood =",x$loglik,"\n")
   
 	cat("\nDependence structure parameter estimates:\n")
 	if (any(abs(x$coefficients)[3:4, ] > 10^(-6))){
