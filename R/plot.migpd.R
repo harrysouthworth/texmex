e29d022cf5465bc63f756a8e2728c2ed7bb10cab
janetheffernan <janetheffernan@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2011-10-10 09:59:36 +0000

    plot.migpd.R : added plot method for class migpd.
    git-svn-id: https://texmex.googlecode.com/svn/pkg@395 e3c61688-98f1-767d-8bab-d104bf98bfd2

---
+++ R/plot.migpd.R
@@ -0,0 +1,27 @@
+`plot.migpd` <-
+function(x, main=c("Probability plot","Quantile plot","Return level plot","Histogram and density"), xlab=rep(NULL,4), nsim=1000, alpha=.05, ... ){
+  
+  if( class(x) != "migpd" ){
+     stop("you need to use an object created by migpd")
+  }
+  
+  if ( !missing( main ) ){
+      if ( length( main ) != 1 & length( main ) != 4 ){
+        stop( "main should have length 1 or 4" )
+      } else if ( length( main ) == 1 ){ 
+        main <- rep( main, 4 ) 
+      }
+  }
+    
+  for( i in 1:length(x$models) ) {
+    plot(x$model[[i]], main= paste(rep(names(x$model[i]),4),main), xlab=xlab,nsim=nsim,alpha=0.05,...)
+  }
+  invisible()
+}
+
+test.plot.migpd <- function(){
+  par(mfrow=c(2,2))
+  mod <- migpd(winter, mqu=.7, penalty = "none")
+  res <- plot(mod)
+  checkEquals(res,NULL,msg="plot.migpd: successful execution")
+} 
