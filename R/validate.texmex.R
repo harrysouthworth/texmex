6d2d9c38c5abc3de5e8b85740c1e171b02524183
harrys <harrys@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2011-11-18 08:28:22 +0000

    validate.texmex again
    git-svn-id: https://texmex.googlecode.com/svn/pkg@419 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/validate.texmex.R
+++ R/validate.texmex.R
@@ -11,12 +11,16 @@ validate.texmex <- function () {
    tests <- objects(wh)[substring(objects(wh), 1, 5) == "test."]
 
    # Create temporary directory to put tests into
-   dir <- file.path(tempdir(), "texmex.tests")
-   cmd <- paste("mkdir", dir)
-   try(system(cmd), silent=TRUE)
+   if (.Platform$OS.type == "windows"){ sep <- "\\" }
+   else { sep <- "/" }
+
+   dir <- file.path(tempdir(), "texmex.tests", fsep = sep)
+#   cmd <- paste("mkdir", dir)
+#   try(system(cmd), silent=TRUE)
+   dir.create(dir)
 
    for (i in 1:length(tests)) {
-       str <- paste(dir, "/", tests[i], ".R", sep = "")
+       str <- paste(dir, sep, tests[i], ".R", sep = "")
        dump(tests[i], file = str)
    }
    res <- defineTestSuite("texmex", dirs = dir, testFuncRegexp = "^test.+", testFileRegexp = "*.R")
