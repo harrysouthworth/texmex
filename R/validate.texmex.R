5bc6ed41150aae5c420c99f94e60b8bd85e0371e
harrys <harrys@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2011-11-18 07:48:16 +0000

    another go at fixing validate.texmex...
    git-svn-id: https://texmex.googlecode.com/svn/pkg@418 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/validate.texmex.R
+++ R/validate.texmex.R
@@ -11,7 +11,7 @@ validate.texmex <- function () {
    tests <- objects(wh)[substring(objects(wh), 1, 5) == "test."]
 
    # Create temporary directory to put tests into
-   dir <- paste(tempdir(), "texmex.tests", sep = "/")
+   dir <- file.path(tempdir(), "texmex.tests")
    cmd <- paste("mkdir", dir)
    try(system(cmd), silent=TRUE)
 
