9ff0f8a5413c937042e0557d73822c9d97a217a4
harrys <harrys@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2011-12-04 18:05:17 +0000

    rl.gpd now sends something back (needs checking)
    git-svn-id: https://texmex.googlecode.com/svn/pkg@432 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/predict.gpd.R
+++ R/predict.gpd.R
@@ -31,19 +31,17 @@ function(object, newdata=NULL, type=c("return level", "link"), se.fit=FALSE, ci.
                   "return level" = rl.gpd(object, M, newdata),
                   "link" = predict.link.gpd(object, newdata, se.fit, ci.fit, alpha)
                   )
- #   attributes(res, "call") <- theCall
-    
     res
 }
 
 ## Linear predictor functions for GPD
 
-predict.link.gpd <- function(object, newdata, se.fit, ci.fit, alpha){
+predict.link.gpd <- function(object, newdata=NULL, se.fit=FALSE, ci.fit=FALSE, alpha=.050){
 
     if (!is.null(newdata)){
         xi.fo <- object$call$xi
         phi.fo <- object$call$phi
-#browser()
+
         X.xi <- if (!is.null(xi.fo)){ model.matrix(as.formula(xi.fo), newdata) }
                 else { matrix(1, nrow(newdata)) }
         X.phi <- if (!is.null(phi.fo)){ model.matrix(as.formula(object$call$phi), newdata) }
@@ -53,18 +51,18 @@ predict.link.gpd <- function(object, newdata, se.fit, ci.fit, alpha){
     else {
         X.xi <- object$X.xi
         X.phi <- object$X.phi
-   }
+    }
 
-    phi <- c(object$coefficients[1:ncol(object$X.phi)] %*% t(object$X.phi))
-    xi <- c(object$coefficients[(ncol(object$X.phi) + 1):length(object$coefficients)] %*% t(object$X.xi))
+    phi <- c(object$coefficients[1:ncol(X.phi)] %*% t(X.phi))
+    xi <- c(object$coefficients[(ncol(X.phi) + 1):length(object$coefficients)] %*% t(X.xi))
 
     res <- cbind(phi, xi)
 
     if (ci.fit){
-        phi.cov <- as.matrix(object$cov[1:ncol(object$X.phi), 1:ncol(object$X.phi)])
-        xi.cov <- as.matrix(object$cov[(ncol(object$X.phi) + 1):length(object$coefficients), (ncol(object$X.phi) + 1):length(object$se)])
-        phi.se <- sqrt(rowSums((object$X.phi %*% phi.cov) * object$X.phi))
-        xi.se <- sqrt(rowSums((object$X.xi %*% xi.cov) * object$X.xi))
+        phi.cov <- as.matrix(object$cov[1:ncol(X.phi), 1:ncol(X.phi)])
+        xi.cov <- as.matrix(object$cov[(ncol(X.phi) + 1):length(object$coefficients), (ncol(X.phi) + 1):length(object$se)])
+        phi.se <- sqrt(rowSums((X.phi %*% phi.cov) * object$X.phi))
+        xi.se <- sqrt(rowSums((X.xi %*% xi.cov) * X.xi))
 
         z <- qnorm(1 - alpha/2)
 
@@ -78,8 +76,8 @@ predict.link.gpd <- function(object, newdata, se.fit, ci.fit, alpha){
 
     if (se.fit){
         if (!ci.fit){ # Because if ci.fit, phi.se and xi.se already exist
-            phi.se <- sqrt(rowSums((phi %*% phi.cov) * phi))
-            xi.se <- sqrt(rowSums((xi %*% xi.cov) * xi))
+            phi.se <- sqrt(rowSums((X.phi %*% phi.cov) * object$X.phi))
+            xi.se <- sqrt(rowSums((X.xi %*% xi.cov) * X.xi))
         }
         res <- cbind(res, phi.se, xi.se)
     } # Close if(se.fit
@@ -96,16 +94,16 @@ predict.link.gpd <- function(object, newdata, se.fit, ci.fit, alpha){
 ## rl generic
 
 rl <- function(object, M, newdata, ...){
-
     UseMethod("rl")
 }
 
-rl.gpd <- function(object, M, newdata){
-    cat("meow\n")
-    invisible()
+rl.gpd <- function(object, M=1000, newdata){
+    co <- predict.link.gpd(object, newdata=newdata)
+    
+    res <- object$threshold + (exp(co[,1]) / co[,2]) * (object$rate^co[,2] - 1)
+
+    invisible(res)
 }
-
-
 
 ################################################################################
 ## bgpd
