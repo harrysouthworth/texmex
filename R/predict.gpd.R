458adfe80fdf9cdfeb991ed2c4224bb12a490d1f
harrys <harrys@e3c61688-98f1-767d-8bab-d104bf98bfd2>
2011-12-04 17:40:19 +0000

    predict.link.gpd can now cope with newdata :-)
    git-svn-id: https://texmex.googlecode.com/svn/pkg@431 e3c61688-98f1-767d-8bab-d104bf98bfd2

--- R/predict.gpd.R
+++ R/predict.gpd.R
@@ -1,19 +1,20 @@
 # Author: Harry Southworth
 # Date: 2011-11-25
-# Purpose: Create a predict method for objects of class gpd and bgpd that
-#          returns parameters, return levels or (maybe) return periods, 
-#          depending on arguments given.
+## Purpose: Create a predict method for objects of class gpd and bgpd that
+##          returns parameters, return levels or (maybe) return periods, 
+##          depending on arguments given.
 #
 # predict.gpd
 # predict.bgpd
 # predict.bootgpd
+# predict.link.gpd
+# rl
+# rl.gpd
+# predict.link.bgpd
+# rl.bgpd
+# predict.link.bootgpd
+# rl.bootgpd
 
-################################################################################
-
-
-
-################################################################################
-## Need methods for gpd, bgpd, bootgpd.
 
 ################################################################################
 ## gpd
@@ -39,46 +40,49 @@ function(object, newdata=NULL, type=c("return level", "link"), se.fit=FALSE, ci.
 
 predict.link.gpd <- function(object, newdata, se.fit, ci.fit, alpha){
 
-    if (is.null(newdata)){
-        phi <- c(object$coefficients[1:ncol(object$X.phi)] %*% t(object$X.phi))
-        xi <- c(object$coefficients[(ncol(object$X.phi) + 1):length(object$coefficients)] %*% t(object$X.xi))
-
-        res <- cbind(phi, xi)
-
-        if (ci.fit){
-            phi.cov <- as.matrix(object$cov[1:ncol(object$X.phi), 1:ncol(object$X.phi)])
-            xi.cov <- as.matrix(object$cov[(ncol(object$X.phi) + 1):length(object$coefficients), (ncol(object$X.phi) + 1):length(object$se)])
-
-            phi.se <- sqrt(rowSums((object$X.phi %*% phi.cov) * object$X.phi))
-            xi.se <- sqrt(rowSums((object$X.xi %*% xi.cov) * object$X.xi))
-
-            z <- qnorm(1 - alpha/2)
-
-            phi.lo <- phi - phi.se*z
-            phi.hi <- phi + phi.se*z
-            xi.lo <- xi - xi.se*z
-            xi.hi <- xi + xi.se*z
-
-            res <- cbind(res, phi.lo, phi.hi, xi.lo, xi.hi)
-
-        }
-
-        if (se.fit){
-	    if (!ci.fit){ # Because if ci.fit, phi.se and xi.se already exist
-                phi.se <- sqrt(rowSums((phi %*% phi.cov) * phi))
-                xi.se <- sqrt(rowSums((xi %*% xi.cov) * xi))
-            }
-            res <- cbind(res, phi.se, xi.se)
-        }
-
-
+    if (!is.null(newdata)){
+        xi.fo <- object$call$xi
+        phi.fo <- object$call$phi
+#browser()
+        X.xi <- if (!is.null(xi.fo)){ model.matrix(as.formula(xi.fo), newdata) }
+                else { matrix(1, nrow(newdata)) }
+        X.phi <- if (!is.null(phi.fo)){ model.matrix(as.formula(object$call$phi), newdata) }
+                 else { matrix(1, nrow(newdata)) }
     }
 
-    else { 
-        cat("not got that far yet!\n")
-        phi <- NULL; xi <- NULL
-    }
+    else {
+        X.xi <- object$X.xi
+        X.phi <- object$X.phi
+   }
 
+    phi <- c(object$coefficients[1:ncol(object$X.phi)] %*% t(object$X.phi))
+    xi <- c(object$coefficients[(ncol(object$X.phi) + 1):length(object$coefficients)] %*% t(object$X.xi))
+
+    res <- cbind(phi, xi)
+
+    if (ci.fit){
+        phi.cov <- as.matrix(object$cov[1:ncol(object$X.phi), 1:ncol(object$X.phi)])
+        xi.cov <- as.matrix(object$cov[(ncol(object$X.phi) + 1):length(object$coefficients), (ncol(object$X.phi) + 1):length(object$se)])
+        phi.se <- sqrt(rowSums((object$X.phi %*% phi.cov) * object$X.phi))
+        xi.se <- sqrt(rowSums((object$X.xi %*% xi.cov) * object$X.xi))
+
+        z <- qnorm(1 - alpha/2)
+
+        phi.lo <- phi - phi.se*z
+        phi.hi <- phi + phi.se*z
+        xi.lo <- xi - xi.se*z
+        xi.hi <- xi + xi.se*z
+
+        res <- cbind(res, phi.lo, phi.hi, xi.lo, xi.hi)
+    } # Close if(ci.fit
+
+    if (se.fit){
+        if (!ci.fit){ # Because if ci.fit, phi.se and xi.se already exist
+            phi.se <- sqrt(rowSums((phi %*% phi.cov) * phi))
+            xi.se <- sqrt(rowSums((xi %*% xi.cov) * xi))
+        }
+        res <- cbind(res, phi.se, xi.se)
+    } # Close if(se.fit
 
     res
 }
