ac35bf78c4fc976d9cc413f16904727047517a45
jannypi314@gmail.com <jannypi314@gmail.com>
2013-02-27 20:24:38 +0000

    pgev.R : corrected definition for xi=0 to avoid div by zero

--- R/pgev.R
+++ R/pgev.R
@@ -1,4 +1,8 @@
 pgev <- function(q, mu, sigma, xi, lower.tail=TRUE, log.p=FALSE){
     tx <- (1 + xi/sigma * (q - mu))^(-1/xi)
+    
+    tol <- 0.0001
+    tx[abs(xi) < tol] <- exp(-(q-mu)/sigma)
+    
     exp(-tx)
 }
