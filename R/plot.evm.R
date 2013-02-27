cc4c636f29f9ae5955002aa99bf88d29888cca05
jannypi314@gmail.com <jannypi314@gmail.com>
2013-02-27 20:27:43 +0000

    plot.evm.R : replaced legacy code still set up for GPD with evm implementation; extended test to include test for GPD+covariates, GEV and GEV+covariates

--- R/plot.evm.R
+++ R/plot.evm.R
@@ -7,15 +7,16 @@ function(x, main=rep(NULL,4), xlab=rep(NULL,4), nsim=1000, alpha=.05, ...){
         else if (length(main) == 1){ main <- rep(main, 4) }
     }
 
-    if (ncol(x$data$D$phi) == 1 && ncol(x$data$D$xi) == 1){
+    if (all(sapply(x$data$D,ncol) == 1)){
         ppevm(x, main=main[1], xlab=xlab[1], nsim=nsim, alpha=alpha)
         qqevm(x, main=main[2], xlab=xlab[2], nsim=nsim, alpha=alpha)
         plotrl.evmOpt(x, main=main[3], xlab=xlab[3], ...)
         hist.evmOpt(x, main=main[4], xlab=xlab[4])
-    }
-    else { # Covariates in the model
-        fittedScale <- exp(coef(x)[1:ncol(x$data$D$phi)] %*% t(x$data$D$phi))
-        fittedShape <- coef(x)[(ncol(x$data$D$phi) + 1):length(coef(x))] %*% t(x$data$D$xi)
+    } else { # Covariates in the model
+    
+        np <- length(x$data$D)
+        lp <- predict(x,type="lp")
+        Which <- as.logical(apply(lp[,1:np],2,var)) # identifies which cols have covariates
 
         x$data$y <- resid(x)
         x$threshold <- 0
@@ -23,20 +24,14 @@ function(x, main=rep(NULL,4), xlab=rep(NULL,4), nsim=1000, alpha=.05, ...){
         ppevm(x, main=main[1], xlab=xlab[1], nsim=nsim, alpha=alpha)
         qqevm(x, main=main[2], xlab=xlab[2], nsim=nsim, alpha=alpha)
 
-        if(ncol(x$data$D[[1]]) > 1){ # phi
-          plot(fittedScale,resid(x),
-               main="Residuals vs Fitted Scale",
-               xlab="Fitted scale",ylab="Residuals")
-          panel.smooth(fittedScale, resid(x), col.smooth=2)
-        }
-        if(ncol(x$data$D[[2]]) > 1){ # xi
-          plot(fittedShape,resid(x),
-               main="Residuals vs Fitted Shape",
-               xlab="Fitted shape",ylab="Residuals")
-          panel.smooth(fittedShape, resid(x), col.smooth=2)
+        
+        for(i in (1:length(x$data$D))[Which]){
+          ParName <- names(x$data$D[i])
+          plot(lp[,i],resid(x),main=paste("Residuals vs fitted",ParName),xlab=paste("Fitted",ParName),ylab="Residuals")
+          panel.smooth(lp[,i], resid(x), col.smooth=2)
         }
     }
-
+    
     invisible()
 }
 
@@ -46,12 +41,33 @@ test.plot.evmOpt <- function(){
   res <- plot(mod,main=paste(rep("Figure 4.5 of Coles (2001)",4),
               c("\nProbability plot","\nQuantile Plot","\nReturn Level Plot\n(SCALE IS DAYS NOT YEARS)","\nDensity Plot")), 
               RetPeriodRange=c(3.65,365*10000))
-  checkEquals(res,NULL,msg="plot.gpd: successful execution")
+  checkEquals(res,NULL,msg="plot.evmOpt: successful execution")
 
   # check for very short tailed data
   set.seed(6)
   temp <- rgpd(1000,sigma=1,xi=-0.45)
   fit <- evm(temp,th=0)
   res <- plot(fit,main=c("PP","QQ","RL","Hist, Short tailed data"))
-  checkEquals(res,NULL,msg="plot.gpd: successful execution, short tailed data")
+  checkEquals(res,NULL,msg="plot.evmOpt: successful execution, short tailed data")
+  
+  # check for covariates in the model
+  # GPD
+  n <- 1000
+  sig <- 2
+  X <- data.frame(a = rnorm(n),b = runif(n,-0.3,0.3))
+  Y <- rgpd(n,sig,X[,2])
+  X$Y <- Y
+  fit <- evm(Y,data=X,xi=~b,th=0)
+  par(mfrow=c(2,2))
+  res <- plot(fit)
+  checkEquals(res,NULL,msg="plot.evmOpt: GPD with covariates successful execution")
+ 
+  #GEV 
+  X <- data.frame(a = rnorm(n),b = runif(n,-0.3,0.3),C= runif(n))
+  Y <- rgev(n,X[,3],sig,X[,2])
+  X$Y <- Y
+  fit <- evm(Y,data=X,xi=~b,mu=~C,family=gev)
+  par(mfrow=c(2,2))
+  res <- plot(fit,main=rep("GEV with covariates",4))
+  checkEquals(res,NULL,msg="plot.evmOpt: GEV with covariates successful execution")
 }
