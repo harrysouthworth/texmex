a0d3eb49146f30799629ee16822e4b53a375f672
Paul Metcalfe <paul.metcalfe+texmex@gmail.com>
2013-04-04 08:28:13 +0100

    Add in exprel.
    Add in C code to accurately compute (exp(x) - 1) / x.  This will enable
    simplifications of various bits of code.

--- src/texmex.c
+++ src/texmex.c
@@ -70,12 +70,67 @@ void dgpd(double* res,
   }
 }
 
+/* accurately compute (exp(x) - 1) / x */
+
+static inline
+double true_dexprl(const double x) {
+  if (R_FINITE(x)) {
+    /* actual value */
+    if (x != 0) {
+      return expm1(x) / x;
+    } else {
+      return 1;
+    }
+  }
+  else if (ISNA(x)) {
+    return NA_REAL;
+  }
+  else if (ISNAN(x)) {
+    return R_NaN;
+  }
+  else if (x == R_PosInf) {
+    return R_PosInf;
+  }
+  else if (x == R_NegInf) {
+    return 0;
+  } else {
+    /* can't happen */
+    return NA_REAL;
+  }
+}
+
+/* wrap that as an R function */
+
+static
+SEXP dexprl(SEXP x) {
+  double *xa, *ra;
+  SEXP res;
+  int n;
+  PROTECT(x = coerceVector(x, REALSXP));
+  n = length(x);
+  PROTECT(res = allocVector(REALSXP, n));
+  xa = REAL(x);
+  ra = REAL(res);
+  for (;n;--n) {
+    *ra = true_dexprl(*xa);
+    ++xa; ++ra;
+  }
+  UNPROTECT(2);
+  return res;
+}
+
+
 static R_CMethodDef cMethods[] = {
   {".c.rgpd", (DL_FUNC) &rgpd, 5},
   {".c.dgpd", (DL_FUNC) &dgpd, 7},
   NULL
 };
 
+static R_CallMethodDef callMethods[] = {
+  {".c.dexprl", (DL_FUNC) &dexprl, 1},
+  NULL
+};
+
 void R_init_texmex(DllInfo *info) {
-  R_registerRoutines(info, cMethods, NULL, NULL, NULL);
+  R_registerRoutines(info, cMethods, callMethods, NULL, NULL);
 }
