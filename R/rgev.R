0cabd6901d3624c3c3f4b5ba293063f3cb4cce89
Paul Metcalfe <paul.metcalfe+texmex@gmail.com>
2013-04-09 08:17:48 +0100

    Ensure that the distribution parameters are at most n long
    This ensures that each of the return values of rgpd is an independent
    random number.

--- R/rgev.R
+++ R/rgev.R
@@ -5,6 +5,16 @@ rgev <- function(n, mu, sigma, xi){
 
   neg.log.exp <- -log(rexp(n))
 
+  ## expand mu, sigma, and xi to be n long
+  ## this is necessary to ensure that we get
+  ## exactly n random numbers if mu, sigma, xi
+  ## are greater than n long
+  n     <- length(neg.log.exp)
+  mu    <- rep(mu, length.out=n)
+  sigma <- rep(sigma, length.out=n)
+  xi    <- rep(xi, length.out=n)
+
+  ## and here we go
   standard.gev <- .exprel(neg.log.exp * xi) * neg.log.exp
 
   mu + sigma * standard.gev
