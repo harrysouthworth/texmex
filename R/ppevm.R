b5173291c5970946f7973a6fde150d6c747d3b87
Harry Southworth <harry.southworth@gmail.com>
2013-04-24 15:45:33 +0100

    Split PP-plotting into computations and then plot

--- R/ppevm.R
+++ R/ppevm.R
@@ -1,8 +1,5 @@
 ppevm <-
-function(object , nsim = 1000, alpha = .050,
-		 xlab, ylab,  main, # labels and titles
-		 pch=1, col = 2, cex = .75, linecol = 4 ,
-		 intcol = 0, polycol=15){
+function(object , nsim = 1000, alpha = .050){
     # Want parameters as a matrix with one row for passing
     # into family$rng and so on
     a <- t(object$coefficients)
@@ -11,10 +8,6 @@ function(object , nsim = 1000, alpha = .050,
 
     pfun <- object$family$prob
     rfun <- object$family$rng
-
-    if (missing(xlab) || is.null(xlab) ) { xlab <- "Model" }
-    if (missing(ylab) || is.null(ylab) ) { ylab <- "Empirical" }
-    if (missing(main) || is.null(main) ) { main <- "Probability Plot" }
 
     ModPoints <- ppoints(dat)
 
@@ -27,25 +20,40 @@ function(object , nsim = 1000, alpha = .050,
         sim <- apply(sim, 1, quantile, prob = c(alpha/2, 1 - alpha/2))
     }
     else { sim <- NULL }
+
+    res <- list(ModPoints=ModPoints, sim=sim, pfun=pfun, rfun=rfun, dat=dat, a=a, model=object)
+    oldClass(res) <- 'ppevm'
+    res
+}
+
+plot.ppevm <- function(x, xlab, ylab,  main,
+                       pch=1, col = 2, cex = .75, linecol = 4,
+                       intcol = 0, polycol=15, ...){
+
+    if (missing(xlab) || is.null(xlab) ) { xlab <- "Model" }
+    if (missing(ylab) || is.null(ylab) ) { ylab <- "Empirical" }
+    if (missing(main) || is.null(main) ) { main <- "Probability Plot" }
   
     oldpar <- par(pty = "s"); on.exit(oldpar)
-    plot(ModPoints, pfun(sort(dat), a, object),
+
+    plot(x$ModPoints, x$pfun(sort(x$dat), x$a, x$model),
          xlab = xlab, ylab = ylab, main = main, type = "n")
 
 	# If doing the envelope, plot it before putting the data on 
     if (nsim > 0){
         if (polycol != 0)
-            polygon(c(ModPoints, rev(ModPoints)), c(sim[1, ], rev(sim[2, ])),
+            polygon(c(x$ModPoints, rev(x$ModPoints)), c(x$sim[1, ], rev(x$sim[2, ])),
                     col=polycol, border=FALSE)
-        lines(ModPoints, sim[1, ], col = intcol)
-        lines(ModPoints, sim[2, ], col = intcol)
+        lines(x$ModPoints, x$sim[1, ], col = intcol)
+        lines(x$ModPoints, x$sim[2, ], col = intcol)
     }
 
     abline(0, 1, col = linecol)
-    points(ModPoints,
-           pfun(sort(dat), a, object),
+    points(x$ModPoints,
+           x$pfun(sort(x$dat), x$a, x$model),
            pch = pch , col = col, cex = cex)
     box()
     invisible(sim)
 }
 
+print.ppevm <- plot.ppevm
