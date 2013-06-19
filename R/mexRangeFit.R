93349555badeab161fdc270ed340c449c824ddce
Janet Heffernan <jannypi314@gmail.com>
2013-06-19 10:09:59 +0100

    mexRangeFit.R : re-factored to separate engine from plotting functionality, as per gpdRangeFit.  Extended tests to test numerical output more strongly than previously.

--- R/mexRangeFit.R
+++ R/mexRangeFit.R
@@ -1,6 +1,6 @@
 `mexRangeFit` <-
 function (x, which, quantiles=seq(0.5,0.9,length=9), start=c(.01, .01), R=10, nPass=3, trace=10,
-          col=2,bootcol="grey",margins="laplace", constrain=TRUE, v=10, addNexcesses=TRUE,...)
+          margins="laplace", constrain=TRUE, v=10)
 {
   if (class(x) == "mex"){
     if( (!missing(margins))){
@@ -38,12 +38,37 @@ function (x, which, quantiles=seq(0.5,0.9,length=9), start=c(.01, .01), R=10, nP
                                 bootmex(x=X, R=R, nPass=nPass, trace=trace),
                  R=R, nPass=nPass, trace=trace)
 
+  res <- list(ests=ests,boot=boot,quantiles=quantiles)
+  oldClass(res) <- "mexRangeFit"
+  res
+}
+ 
+show.mexRangeFit <-
+  print.mexRangeFit <- function(x, ...){
+    out <- list(a = sapply(x$ests,function(x)x$dependence$coefficients[1,]),
+                b = sapply(x$ests,function(x)x$dependence$coefficients[2,]))
+    colnames(out$a) <- colnames(out$b) <- x$quantiles
+    print(out)
+    invisible()
+  }
+
+summary.mexRangeFit <- function(object, ...){
+  print(object)
+}
+
+plot.mexRangeFit <- function(x,col=2,bootcol="grey",addNexcesses=TRUE,...){
+  ests <- x$ests
+  boot <- x$boot
+  quantiles <- x$quantiles
   PointEsts <- sapply(ests,function(X) coef(X$dependence))
   cof <- coef(ests[[1]]$dependence)
   whichName <- ests[[1]]$dependence$conditioningVariable
-  Names <- paste(rep(rownames(cof),dim(x$data)[2]-1),
+  which <- ests[[1]]$dependence$which
+  data <- ests[[1]]$margins$data
+  Names <- paste(rep(rownames(cof),dim(data)[2]-1),
                  paste(rep(colnames(cof),each=6),whichName,sep=" | "),sep="  ")
-
+  R <- length(boot[[1]]$boot)
+  
   for(i in 1:dim(PointEsts)[1]){
     if( sum((i%%6) == 1:4) ){ # exclude plots from nuisance parameters m and s for which i mod 6 = 5,0 resp
       if(sum(PointEsts[i,])){
@@ -53,7 +78,7 @@ function (x, which, quantiles=seq(0.5,0.9,length=9), start=c(.01, .01), R=10, nP
         points(rep(quantiles,each=R),Boot,col=bootcol)
         points(quantiles,PointEsts[i,],col=col)
         if(addNexcesses){
-          axis(3,at=axTicks(1),labels=sapply(axTicks(1),function(u,dat,which)sum(dat[,which] > quantile(dat[,which],u)), dat=x$data,which=which))
+          axis(3,at=axTicks(1),labels=sapply(axTicks(1),function(u,dat,which)sum(dat[,which] > quantile(dat[,which],u)), dat=data,which=which))
           mtext("# threshold excesses")
         }
       }
@@ -63,35 +88,42 @@ function (x, which, quantiles=seq(0.5,0.9,length=9), start=c(.01, .01), R=10, nP
 
 test.mexRangeFit <- function(){
 
+  which <- 2
+  quantiles <- seq(0.5,0.9,length=5)
   wmarmod <- migpd(winter, mqu=.7,  penalty="none")
-  wmexmod.gum <- mex(winter, mqu=.7,  penalty="none", margins="gumbel", constrain=FALSE, which=1)
-  wmexmod.lap <- mex(winter, mqu=.7,  penalty="none", margins="laplace",v=5, which=1)
+  wmexmod.gum <- mexDependence(wmarmod, dqu=quantiles[1], margins="gumbel", constrain=FALSE,which=which)
+  wmexmod.lap <- mexDependence(wmarmod, dqu=quantiles[1], margins="laplace",v=5,which=which)
 
-  par(mfrow=c(2,2))
-  R <- 10
-  mexRangeFit(wmarmod,which=1,margins="gumbel",constrain=FALSE,R=R,trace=R+1,
-              main="Dependence threshold selection\nWinter data, Heffernan and Tawn 2004",cex=0.5,addNexcesses=FALSE)
-  mexRangeFit(wmexmod.gum,main="Dependence threshold selection\nWinter data, Heffernan and Tawn 2004,\nGumbel margins",cex=0.5,addNexcesses=FALSE,R=R,trace=R+1)
-  mexRangeFit(wmexmod.lap,main="Dependence threshold selection\nWinter data, Heffernan and Tawn 2004,\nLaplace margins",cex=0.5,addNexcesses=FALSE,R=R,trace=R+1)
+  R <- 3
+  mrf1 <- mexRangeFit(wmarmod,quantiles = quantiles,which=which,R=R,trace=R+1,v=5)
+  mrf2 <- mexRangeFit(wmexmod.gum,quantiles = quantiles,R=R,trace=R+1)
+  mrf3 <- mexRangeFit(wmexmod.lap,quantiles = quantiles,R=R,trace=R+1)
 
   checkException(mexRangeFit(TRUE,which=2),silent=TRUE,msg="mexRangeFit: exception handle")
   checkException(mexRangeFit(5,which=1),silent=TRUE,msg="mexRangeFit: exception handle")
-
+  
+  checkEquals(mrf1$ests[[1]][1:2],wmexmod.lap[1:2])
+  checkEquals(mrf2$ests[[1]][1:2],wmexmod.gum[1:2])
+  checkEquals(mrf3$ests[[1]][1:2],wmexmod.lap[1:2])
+  
 # now 2-d data
 
-  wavesurge.fit <- migpd(wavesurge,mqu=.7)
-  m <- mex(wavesurge,which=1,mqu=0.7)
-  mexRangeFit(wavesurge.fit,which=1,margins="laplace",R=R,trace=R+1,
-              main="Dependence threshold selection,\nwave and surge data, Coles 2001",addNexcesses=FALSE)
-  mexRangeFit(wavesurge.fit,which=1,margins="gumbel",constrain=FALSE,R=R,trace=R+1,
-              main="Dependence threshold selection,\nwave and surge data, Coles 2001",addNexcesses=FALSE)
+  mqu <- .7
+  wavesurge.fit <- migpd(wavesurge,mqu=mqu)
+  m <- mexDependence(wavesurge.fit,which=1,dqu=mqu)
+  mrf4 <- mexRangeFit(wavesurge.fit,which=1,margins="laplace",R=R,trace=R+1)
+  mrf5 <- mexRangeFit(m,R=R,trace=R+1)
+  checkEquals(mrf4$ests[[2]][1:2],mrf5$ests[[2]][1:2])
 
 # test specification of starting values
   R <- 5
   qu <- c(0.5,0.7,0.9)
-  mexRangeFit(wavesurge.fit,which=1,margins="laplace",constrain=TRUE, start=c(0.01,0.01),R=R,trace=R+1,quantiles = qu,
-              main="start=c(0.01,0.01)",addNexcesses=FALSE)
-  mexRangeFit(wavesurge.fit,which=2,margins="laplace",constrain=TRUE, start=m,R=R,trace=R+1,quantiles = qu,
-              main="start=fitted model",addNexcesses=FALSE)
-
+  mrf6 <- mexRangeFit(wavesurge.fit,which=1,margins="laplace",constrain=TRUE, start=c(0.01,0.01),R=R,trace=R+1,quantiles = qu)
+  mrf7 <- mexRangeFit(wavesurge.fit,which=2,margins="laplace",constrain=TRUE, start=m,R=R,trace=R+1,quantiles = qu)
+  
+# test plotting
+  
+  par(mfrow=c(2,2))
+  plot(mrf6,main="start=(0.01,0.01)",addNexcesses=FALSE)
+  plot(mrf7,main=paste("start=",signif(coef(m)$dependence[1:2],2)),addNexcesses=FALSE)
 }
