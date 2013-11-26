990bbb1a23d4f9459e27dccabda022d8da51e4bc
harry <harry.southworth@gmail.com>
2013-11-26 13:49:54 +0000

    adding vignette build script

--- vignettes/build.sh
+++ vignettes/build.sh
@@ -1,11 +1,11 @@
-#! /bin/tcsh
+#! /bin/bash
 
-cd /home/kpzv097/googlecode/texmex/vignettes
+cd /home/harry/Work/repos/googlecode/texmex/vignettes
 mv declustering.knitr declustering.Rnw
 mv texmex1d.knitr texmex1d.Rnw
 mv texmexMultivariate.knitr texmexMultivariate.Rnw
 
-R < build.R
+R < build.R --no-save
 
 mv declustering.Rnw declustering.knitr
 mv texmex1d.Rnw texmex1d.knitr
