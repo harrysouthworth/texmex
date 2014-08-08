70be68bad7a52d5e7b67fe0c4e8dd652cc2c03ce
Paul Metcalfe <paul.metcalfe+texmex@gmail.com>
2014-08-08 10:01:51 +0100

    Directly compute degp3
    Use definition of EGP3 to directly compute degp3 from dgpd and pgpd.
    This simplifies degp3 somewhat.

--- R/degp3.R
+++ R/degp3.R
@@ -1,21 +1,7 @@
 degp3 <- function(x, kappa=1, sigma, xi, u=0, log.d=FALSE){
+    res <- log(kappa) + dgpd(x, sigma, xi, u=u, log.d=TRUE) +
+        (kappa - 1) * pgpd(x, sigma, xi, u=u, lower.tail=TRUE, log.p=TRUE)
 
-  below <- kappa <= 0 | x < u
-  x <- pmax((x - u) / sigma, 0)
-
-  xi.ne.0 <- function(x, k, xi, sigma){
-      y <- 1 + x * xi
-      log(k/sigma) + (k - 1) * log(1 - y^(-1/xi)) - (1/xi + 1) * log(y)
-  }
-  xi.eq.0 <- function(x, k, xi, sigma){
-      log(k/sigma) + (k - 1) * log(1 - exp(-x)) - x
-  }
-
-  # Let NaNs be NaNs. Suppress warnings
-  res <- suppressWarnings(ifelse(xi == 0, xi.eq.0(x, kappa, xi, sigma), xi.ne.0(x, kappa, xi, sigma)))
-
-  res[below] <- -Inf
-
-  if (log.d) res
-  else exp(res)
+    if (log.d) res
+    else exp(res)
 }
