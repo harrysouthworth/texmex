925ed0a0784139b50a23ac781014f2c4728344c5
Paul Metcalfe <paul.metcalfe+texmex@gmail.com>
2014-08-08 13:10:04 +0100

    Simplify logic in regp3
    regp3 was essentially a straight copy of qegp3; here we make that
    explicit.

--- R/regp3.R
+++ R/regp3.R
@@ -5,22 +5,7 @@ regp3 <- function(n, kappa=1, sigma, xi, u=0){
   xi <- rep(xi, length.out=n)
   u <- rep(u, length.out=n)
 
-  nk1 <- sum(kappa != 1)
-
-  x <- 1 - runif(nk1)^(1/kappa)
-  
-  xi.neq.0 <- function(x, xi, sigma, u) (x^(-xi) - 1) * sigma / xi + u
-  xi.eq.0 <- function(x, xi, sigma, u) log(x) * sigma / xi + u
-  
-  res <- ifelse(xi == 0, xi.eq.0(x, xi, sigma, u), xi.neq.0(x, xi, sigma, u))
-  
-  if (nk1 < n){
-    res2 <- rgpd(n - nk1, sigma, xi, u)
-    r <- rep(0, n)
-    r[kappa != 1] <- res
-    r[kappa == 1] <- res2
-    r
-  } else res
+  qegp3(-rexp(n), kappa, sigma, xi, u, log.p=TRUE)
 }
 
 
