31e2cb969934cce0928dcb92c3ce3775c38839ef
Harry <harry.southworth@gmail.com>
2014-09-03 11:05:57 +0100

    Fixing collate field, now passing test_pacakge

--- R/revTransform.R
+++ R/revTransform.R
@@ -11,20 +11,21 @@ function (x, data, qu, th = 0, sigma = 1, xi = 0, method = "mixture") {
                 }, 0, p=probs)
    px <- as.integer(round(px * (1 + n)))
    res <- sort(data)[px]
-
-   # Real data contain ties which can cause x[res > th] < qu, res[res < th] > qu
-   i.x <- x >= qu
-   i.r <- res > th
-   i.rx <- apply(cbind(i.x, i.r), 1, all)
    
-   if (method == "mixture" & sum(i.rx > 0)){
-     wh <- u2gpd(x[i.rx], p=1-qu, th=th, sigma=sigma, xi=xi)
-     rth <- res[i.rx]
-     o <- order(rth)
-     rth <- rth[o]
-     rth[length(rth):(length(rth) - length(wh) + 1)] <- rev(sort(wh))
-     rth <- rth[order(o)]
-     res[i.rx] <- rth
+   if (method == "mixture"){
+     # Real data contain ties which can cause x[res > th] < qu, res[res < th] > qu
+     i.x <- x >= qu
+     i.r <- res > th
+     i.rx <- apply(cbind(i.x, i.r), 1, all)
+     if (sum(i.rx > 0)){
+       wh <- u2gpd(x[i.rx], p=1-qu, th=th, sigma=sigma, xi=xi)
+       rth <- res[i.rx]
+       o <- order(rth)
+       rth <- rth[o]
+       rth[length(rth):(length(rth) - length(wh) + 1)] <- rev(sort(wh))
+       rth <- rth[order(o)]
+       res[i.rx] <- rth
+     }
    }
 
    res[order(x)] <- sort(res)
