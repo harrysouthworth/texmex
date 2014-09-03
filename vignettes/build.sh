1d692dd2b73369a7dbaaa3658c0598540180bab4
Harry <harry.southworth@gmail.com>
2014-09-03 16:52:11 +0100

    Making vignettes build and appear in documentation. NOTE egp3 vignette is NOT final

--- vignettes/build.sh
+++ vignettes/build.sh
@@ -4,11 +4,15 @@ cd /home/harry/Work/repos/googlecode/texmex/vignettes
 mv declustering.knitr declustering.Rnw
 mv texmex1d.knitr texmex1d.Rnw
 mv texmexMultivariate.knitr texmexMultivariate.Rnw
+mv test_texmex.knitr test_texmex.Rnw
+mv egp3.knitr egp3.Rnw
 
 R < build.R --no-save
 
 mv declustering.Rnw declustering.knitr
 mv texmex1d.Rnw texmex1d.knitr
 mv texmexMultivariate.Rnw texmexMultivariate.knitr
+mv test_texmex.Rnw test_texmex.knitr
+mv egp3.Rnw egp3.knitr
 
 make all
