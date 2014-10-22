897d9c6c980614056357a50ec0d39661d65a4394
Harry Southworth <harry.southworth@gmail.com>
2014-10-22 14:59:01 +0100

    Simulate is broken. Need to think what to do: simulate a version of the original data (so not use most of the simualted parameters), or something else

--- R/simulate.R
+++ R/simulate.R
@@ -4,12 +4,20 @@ simulate.evmOpt <- function(object, nsim=1, seed=NULL, param=NULL, ...) {
   unname(object$family$rng(nsim, param, object))
 }
 
-simulate.evmSim <- function(object, nsim=NULL, seed=NULL, ...) {
+simulate.evmSim <- function(object, nsim=NULL, seed=NULL, ...){
   if (!is.null(seed)) set.seed(seed)
-  simulate(object$map, nrow(object$param), param=object$param, seed=seed)
+
+  param <- predict(object, type="lp", unique.=FALSE)
+  param <- param$link[, colnames(param$link) %in% names(param$family$param)]
+
+  simulate(object$map, nrow(param), param=param, seed=seed)
 }
 
 simulate.evmBoot <- function(object, nsim=NULL, seed=NULL, ...){
   if (!is.null(seed)) set.seed(seed)
-  simulate(object$map, nrow(object$replicates), param=object$replicates, seed=seed)
+
+  param <- predict(object, type="lp", unique.=FALSE)
+  param <- param$link[, colnames(param$link) %in% names(param$family$param)]
+
+  simulate(object$map, nrow(param), param=param, seed=seed)
 }
