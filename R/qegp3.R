e38a45225bb188805069bb8f1847fe2c7dba5376
Harry Southworth <harry.southworth@gmail.com>
2016-10-26 15:58:39 +0100

    Unbreaking qegp3

--- R/qegp3.R
+++ R/qegp3.R
@@ -1,12 +1,6 @@
-#' @rdname degp3
-#' @export
 qegp3 <- function(p, kappa=1, sigma, xi, u=0, lower.tail=TRUE, log.p=FALSE){
   if (lower.tail){
-    p <- if(log.p){
-      log(exp(p / kappa))
-    } else{
-      p^(1/kappa)
-    }
+    p <- if(log.p) {p / kappa} else {p^(1/kappa)}
     qgpd(p, sigma, xi, u, lower.tail=lower.tail, log.p=log.p)
   } else {
       ## transform, very carefully, to the appropriate thing
