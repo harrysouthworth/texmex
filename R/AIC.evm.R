1d57db7b081c772130e739ef928bfcb950be12ed
Harry Southworth <harry.southworth@gmail.com>
2017-07-20 18:54:44 +0100

    Adding DIC and approx DIC

--- R/AIC.evm.R
+++ R/AIC.evm.R
@@ -1,21 +1,59 @@
-#' Akaike's Information Criterion
+#' Information Criteria
 #'
-#' Compute AIC for \code{evmOpt} objects
+#' Compute AIC and (approximate) DIC for \code{evmOpt} objects
 #'
+#' @aliases AIC.evmSim
 #' @param object fit model object
 #' @param penalized whether to use the penalized log-likelihood
+#' @param nsamp Number of approximate Gaussian sample to use in computing DIC.
+#'   Defaults to \code{nsamp=1e3}. Only used when the object has class 'evmOpt'.
 #' @param ... other arguments currently ignored
 #' @param k numeric, the penalty per parameter to be used; the
 #'     default \code{k = 2} is the classical AIC.
-#' @return the AIC
+#' @details If the object has class 'evmOpt', \code{nsamp} random draws are
+#'   made from the Gaussian distribution with mean and covariance inferred from
+#'   the model object. The result will be an approximate DIC. Note that AIC should
+#'   not be trusted if priors are not flat. For example, if you use a regularizing
+#'   prior on xi, say xi ~ N(0, 0.25), AIC can be misleading and DIC should be
+#'   preferred. If the object has class 'evmSim', the actual posterior draws are
+#'   used in the computation.
+#' @return The AIC and DIC
 #' @seealso \code{\link[stats]{AIC}}
 #' @importFrom stats AIC logLik
 #' @export
-AIC.evmOpt <- function(object, penalized=FALSE, ..., k=2){
+AIC.evmOpt <- function(object, penalized=FALSE, nsamp=1e3, ..., k=2){
   ll <- unclass(logLik(object, penalized=penalized))
   aic <- -2*ll + k * attr(ll, 'df')
   attr(aic, "df") <- NULL
-  aic
+
+  # Get approximate DIC by sampling from Gaussian
+  samp <- rmvnorm(nsamp, coef(object), object$cov)
+
+  dic <- try(DIC.evm(object, samp), silent=TRUE)
+
+  if (class(dic) == "try-error"){
+    dic <- NA
+  }
+
+  c(AIC=aic, DIC=dic)
+}
+
+DIC.evm <- function(object, samp){
+  ll <- object$family$log.lik(object$data, object$th)
+
+  dev <- -2 * apply(samp, 1, ll)
+  dev <- dev[dev < Inf]
+
+  mean(dev) + (mean(dev) + 2 * ll(colMeans(samp)))
+}
+
+#' @export
+AIC.evmSim <- function(object){
+  samp <- object$param
+
+  res <- DIC.evm(object$map, samp)
+  names(res) <- "DIC"
+  res
 }
 
 #' Log-likelihood for evmOpt objects
@@ -31,8 +69,8 @@ AIC.evmOpt <- function(object, penalized=FALSE, ..., k=2){
 #' @importFrom stats logLik
 #' @export
 logLik.evmOpt <- function(object, penalized=FALSE, ...){
-    ll <- if (penalized) {object$ploglik} else {object$loglik}
-    attr(ll, 'df') <- length(coef(object))
-    class(ll) <- 'logLik'
-    ll
+  ll <- if (penalized) {object$ploglik} else {object$loglik}
+  attr(ll, 'df') <- length(coef(object))
+  class(ll) <- 'logLik'
+  ll
 }
