c9f34f853167ae8926ad375064cfb00829160c55
Paul Metcalfe <paul.metcalfe@gmail.com>
2018-08-10 18:39:33 +0100

    Enable trivial copula tests

--- tests/testthat/test-copula.R
+++ tests/testthat/test-copula.R
@@ -1,24 +1,21 @@
 context("copula")
 
 test_that("copula behaves as it should", {
-  skip_on_cran()
-  skip_on_travis()
+
     fun <- function(d) apply(d,2,function(x)(1:n)[rank(x)])/(1+n)
-  n <- 200
-  
-  u2 <- cbind(sample(n),sample(n))
-  d2 <- fun(u2)
-  
-  u3 <- cbind(sample(n),sample(n),sample(n))
-  d3 <- fun(u3)
-  
-  expect_that(d2, equals(copula(u2)$copula), label="copula:2dimensional")
-  expect_that(d3, equals(copula(u3)$copula), label="copula:3dimensional")
-  
-  op <- options()
-  options(show.error.messages=FALSE)
-  expect_that(copula(TRUE), throws_error(), label="copula:exception")
-  expect_that(copula("text"), throws_error(), label="copula:exception")
-  options(op)
-}
-)
+    n <- 200
+    
+    u2 <- cbind(sample(n),sample(n))
+    d2 <- fun(u2)
+    
+    u3 <- cbind(sample(n),sample(n),sample(n))
+    d3 <- fun(u3)
+    
+    expect_equal(d2, copula(u2)$copula, label="copula:2dimensional")
+    expect_equal(d3, copula(u3)$copula, label="copula:3dimensional")
+})
+
+test_that("copula throws errors", {
+    expect_error(copula(TRUE), label="copula:exception")
+    expect_error(copula("text"), label="copula:exception")
+})
